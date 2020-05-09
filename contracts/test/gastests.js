56fa89d46f597e0e4906f45580488b7b56f1a94b
Ben DiFrancesco <ben@scopelift.co>
2020-05-08 22:13:07 -0400

    Add gas usage benchmarking files

--- contracts/test/gastests.js
+++ contracts/test/gastests.js
@@ -13,7 +13,7 @@ function randomBytes(length) {
     for ( var i = 0; i < length*2; i++ ) {
        result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
-    return `0x${result}`;
+    return `${result}`;
  }
 
 describe('Gas Tests', () => {
@@ -28,10 +28,31 @@ describe('Gas Tests', () => {
 
         let bytes = [];
         for (let i = 0; i < 7; i++) {
-            bytes.push(randomBytes(32));
+            bytes.push(`0x${randomBytes(32)}`);
         }
 
-        let tx = await this.instance.manyBytes(...bytes, {from: user, gasPrice: 1});
+        let tx = await this.instance.manyBytes(...bytes, {from: user, value: toWei('1', 'ether'), gasPrice: 1});
+        console.log("GAS USED --->", tx.receipt.gasUsed);
+    });
+
+    it('should test the option with one bytes16 param', async () => {
+        console.log("TESTING 6 bytes32 + 1 bytes16 ARGUMENTS");
+
+        let bytes = [];
+        for (let i = 0; i < 6; i++) {
+            bytes.push(`0x${randomBytes(32)}`);
+        }
+
+        bytes.push(`0x${randomBytes(16)}`);
+
+        let tx = await this.instance.halfByte(...bytes, {from: user, value: toWei('1', 'ether'), gasPrice: 1});
+        console.log("GAS USED --->", tx.receipt.gasUsed);
+    });
+
+    it('should test the big string option', async () => {
+        console.log("TESTING BIG string ARGUMENT");
+        let strBytes = randomBytes(208);
+        let tx = await this.instance.bigString(strBytes, {from: user, value: toWei('1', 'ether'), gasPrice: 1});
         console.log("GAS USED --->", tx.receipt.gasUsed);
     });
 });
