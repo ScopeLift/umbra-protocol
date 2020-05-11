8de82b6c150662d6003ff8543e3b6bc7dd30840d
mds <17163988+mds1@users.noreply.github.com>
2020-05-11 12:02:15 -0700

    Configure send page

--- app/src/pages/Send.vue
+++ app/src/pages/Send.vue
@@ -4,13 +4,73 @@
       Send
     </h3>
 
-    <div class="form">
+    <div
+      v-if="!userAddress"
+      class="text-center"
+    >
+      Please login to send funds
+    </div>
+    <div
+      v-else
+      class="form"
+    >
+      <!-- Choose recipient -->
       <lookup-recipient />
+
+      <!-- Select token -->
+      <div>
+        Select the token you'd like to send
+      </div>
+      <q-select
+        v-model="selectedToken"
+        emit-value
+        filled
+        label="Select token"
+        :options="tokenList"
+        option-label="symbol"
+        option-value="symbol"
+      >
+        <template v-slot:option="token">
+          <q-item
+            v-bind="token.itemProps"
+            v-on="token.itemEvents"
+          >
+            <q-item-section avatar>
+              <img
+                :src="token.opt.image"
+                style="max-width:30px; max-height:30px; margin: 0 auto;"
+              >
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>{{ token.opt.symbol }}</q-item-label>
+            </q-item-section>
+          </q-item>
+        </template>
+      </q-select>
+
+      <!-- Enter Amount -->
+      <div class="q-mt-lg">
+        Enter the amount to send
+      </div>
+      <base-input
+        v-model.number="tokenAmount"
+        type="number"
+        label="Enter Amount"
+        :rules="isValidAmount"
+      />
+
+      <!-- Approve/Send Button -->
+      <base-button
+        :disabled="true"
+        :full-width="true"
+        label="TODO: Approve or Send Tx"
+      />
     </div>
   </q-page>
 </template>
 
 <script>
+import { mapState } from 'vuex';
 import LookupRecipient from 'components/LookupRecipient';
 
 export default {
@@ -18,6 +78,39 @@ export default {
 
   components: {
     LookupRecipient,
+  },
+
+  data() {
+    return {
+      selectedToken: undefined,
+      tokenAmount: undefined,
+      tokenList: [
+        {
+          symbol: 'ETH',
+          image: 'statics/tokens/eth.png',
+        },
+        {
+          symbol: 'DAI',
+          image: 'statics/tokens/dai.png',
+        },
+        {
+          symbol: 'USDC',
+          image: 'statics/tokens/usdc.png',
+        },
+      ],
+    };
+  },
+
+  computed: {
+    ...mapState({
+      userAddress: (state) => state.user.userAddress,
+    }),
+  },
+
+  methods: {
+    isValidAmount(val) {
+      return val > 0 ? true : 'Please enter a valid amount';
+    },
   },
 };
 </script>
