7b60da2ae0deeafc1fbe8f99cfa335105005d257
mds <17163988+mds1@users.noreply.github.com>
2020-05-12 17:07:04 -0700

    Add warning to header

--- app/src/layouts/BaseLayout.vue
+++ app/src/layouts/BaseLayout.vue
@@ -4,60 +4,74 @@
       class="q-mx-md q-mt-md"
       style="color: #000000; background-color: rgba(0,0,0,0)"
     >
-      <div class="row justify-between items-center no-wrap all-content-format">
-        <div class="col-auto">
-          <!-- LOGO AND TITLE -->
-          <div
-            class="row justify-start items-center"
-            style="cursor: pointer;"
-            @click="$router.push({ name: 'home' })"
-          >
-            <img
-              alt="Umbra logo"
-              class="q-ml-md"
-              src="statics/icons/favicon-128x128.png"
-              style="max-width: 50px;"
-            >
-            <div class="text-h5 dark-toggle">
-              <span class="primary header-black q-ml-md">Umbra</span>
+      <div class="column all-content-format">
+        <!-- Main header -->
+        <div>
+          <div class="row justify-between items-center no-wrap all-content-format">
+            <div class="col-auto">
+              <!-- LOGO AND TITLE -->
+              <div
+                class="row justify-start items-center"
+                style="cursor: pointer;"
+                @click="$router.push({ name: 'home' })"
+              >
+                <img
+                  alt="Umbra logo"
+                  class="q-ml-md"
+                  src="statics/icons/favicon-128x128.png"
+                  style="max-width: 50px;"
+                >
+                <div class="text-h5 dark-toggle">
+                  <span class="primary header-black q-ml-md">Umbra</span>
+                </div>
+              </div>
+            </div>
+            <!-- ADDRESS AND SETTINGS AND SETTINGS -->
+            <div class="col-auto q-mr-md">
+              <div
+                v-if="userAddress"
+                class="text-caption dark-toggle"
+              >
+                Address: {{ userAddress }}
+              </div>
+              <div class="row justify-end items-center q-mt-xs">
+                <div
+                  v-if="userAddress && !isCorrectNetwork"
+                  class="negative text-bold q-mr-md"
+                >
+                  You must be on the Ropsten network to use this app
+                </div>
+                <q-icon
+                  v-if="!$q.dark.isActive"
+                  class="col-auto dark-toggle"
+                  name="fas fa-moon"
+                  style="cursor: pointer;"
+                  @click="toggleNightMode()"
+                />
+                <q-icon
+                  v-else
+                  class="col-auto dark-toggle"
+                  name="fas fa-sun"
+                  style="cursor: pointer;"
+                  @click="toggleNightMode()"
+                />
+                <connect-wallet
+                  v-if="!userAddress"
+                  class="q-ml-lg"
+                  label="Login"
+                />
+              </div>
             </div>
           </div>
         </div>
-        <!-- ADDRESS AND SETTINGS AND SETTINGS -->
-        <div class="col-auto q-mr-md">
-          <div
-            v-if="userAddress"
-            class="text-caption dark-toggle"
-          >
-            Address: {{ userAddress }}
-          </div>
-          <div class="row justify-end items-center q-mt-xs">
-            <div
-              v-if="userAddress && !isCorrectNetwork"
-              class="negative text-bold q-mr-md"
-            >
-              You must be on the Ropsten network to use this app
-            </div>
-            <q-icon
-              v-if="!$q.dark.isActive"
-              class="col-auto dark-toggle"
-              name="fas fa-moon"
-              style="cursor: pointer;"
-              @click="toggleNightMode()"
-            />
-            <q-icon
-              v-else
-              class="col-auto dark-toggle"
-              name="fas fa-sun"
-              style="cursor: pointer;"
-              @click="toggleNightMode()"
-            />
-            <connect-wallet
-              v-if="!userAddress"
-              class="q-ml-lg"
-              label="Login"
-            />
-          </div>
+        <!-- Alpha warning -->
+        <div
+          id="alpha"
+          class="text-center q-mt-lg"
+        >
+          WARNING: This is alpha software and is only available on Ropsten. For security,
+          do NOT use this site with an account that holds real funds on the mainnet.
+          If using MetaMask, click "Create Account" to create a fresh account for this app.
         </div>
       </div>
     </q-header>
@@ -107,3 +121,13 @@ export default {
   },
 };
 </script>
+
+<style lang="sass" scoped>
+#alpha
+  border: 2px solid $negative
+  border-radius: 15px
+  color: $negative
+  font-weight: bold
+  font-size: 1rem
+  padding: 1rem
+</style>
