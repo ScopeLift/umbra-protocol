3e45686cfa9c48cae2d267ade43f48bffd437c24
Ben DiFrancesco <ben@scopelift.co>
2020-05-18 08:00:05 -0400

    Some initial setup for GSN usage & testing
    * Stub out an Umbra Paymaster contract
    * Fork Kovan when testing for eventual access to GSN
      RelayHub + UniswapV2
    * Set up test scaffolding, including deployment of Umbra
      and paymaster contracts

---
+++ contracts/contracts/UmbraPaymaster.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.6.2;
+pragma experimental ABIEncoderV2;
+
+import "@opengsn/gsn/contracts/BasePaymaster.sol";
+
+contract UmbraPaymaster is BasePaymaster {
+
+    address public umbraAddr;
+
+    constructor(address _umbraAddr) public {
+        umbraAddr = _umbraAddr;
+    }
+
+    function acceptRelayedCall(
+		GSNTypes.RelayRequest calldata relayRequest  ,
+		bytes calldata approvalData,
+		uint256 maxPossibleGas
+	) external view override returns (bytes memory context) {
+		(approvalData, maxPossibleGas);  // avoid a warning
+
+		require(relayRequest.target == umbraAddr);
+
+		return abi.encode(now);
+	}
+
+    function preRelayedCall(
+		bytes calldata context
+	) external relayHubOnly override returns(bytes32) {
+		return bytes32(0);
+	}
+
+	function postRelayedCall(
+		bytes calldata context,
+		bool success,
+		bytes32 preRetVal,
+		uint256 gasUse,
+		GSNTypes.GasData calldata gasData
+	) external relayHubOnly override {
+		(success, preRetVal, gasUse, gasData);
+	}
+}
--- contracts/test-environment.config.js
+++ contracts/test-environment.config.js
@@ -1,1 +1,9 @@
-module.exports = { };
+require('dotenv').config();
+const infura_key = process.env.INFURA_ID;
+
+module.exports = {
+    node: {
+      fork: `https://kovan.infura.io/v3/${infura_key}`,
+      //unlocked_accounts: [process.env.DAI_FUNDER],
+    },
+  };
