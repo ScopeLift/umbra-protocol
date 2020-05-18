5aac331f7dcd0ac3fa1d4502669636b1092be5db
Ben DiFrancesco <ben@scopelift.co>
2020-05-18 12:56:49 -0400

    Complete GSN Implementation And Test Token Withdrawls

--- contracts/test-environment.config.js
+++ contracts/test-environment.config.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
-const infura_key = process.env.INFURA_ID;
+//const infura_key = process.env.INFURA_ID;
 
 module.exports = {
-    node: {
-      fork: `https://kovan.infura.io/v3/${infura_key}`,
-      //unlocked_accounts: [process.env.DAI_FUNDER],
-    },
+    // node: {
+    //   fork: `https://kovan.infura.io/v3/${infura_key}`,
+    //   //unlocked_accounts: [process.env.DAI_FUNDER],
+    // },
   };
