4096b0c40bb0307fc0960d6ac2026067388a52ae
Ben DiFrancesco <ben@scopelift.co>
2020-05-20 07:12:50 -0400

    Revert unneeded changes to test-environment.config.js

--- contracts/test-environment.config.js
+++ contracts/test-environment.config.js
@@ -1,9 +1,1 @@
-require('dotenv').config();
-//const infura_key = process.env.INFURA_ID;
-
-module.exports = {
-    // node: {
-    //   fork: `https://kovan.infura.io/v3/${infura_key}`,
-    //   //unlocked_accounts: [process.env.DAI_FUNDER],
-    // },
-  };
+module.exports = { };
