0ab8407c593b6a4a33919c90973d7a80a5f43daa
mds <17163988+mds1@users.noreply.github.com>
2020-05-20 19:36:03 -0700

    Implement send functionality

--- app/src/pages/Send.vue
+++ app/src/pages/Send.vue
@@ -67,10 +67,34 @@
       <!-- Approve/Send Button -->
       <base-button
         :disabled="!isDataValid"
+        :loading="isSendPending"
         :full-width="true"
         label="Send"
-        @click="sendFunds"
+        @click="sendFundsFlow"
       />
+
+      <div
+        v-if="selectedToken && selectedToken !== 'ETH' && !hasAllowance"
+        class="text-caption q-mt-md"
+      >
+        Since this is your first time sending {{ selectedToken }}, you will be asked
+        to send two transactions. The first grants Umbra approval to spend your
+        tokens, and the second is the payment.
+      </div>
+
+      <div
+        v-if="isTxProcessing"
+        class="q-mt-md"
+      >
+        Your transaction is processing...
+        <div class="text-caption">
+          <a
+            :href="`https://ropsten.etherscan.io/tx/${txHash}`"
+            target="_blank"
+            class="hyperlink"
+          >View on Etherscan</a>
+        </div>
+      </div>
     </q-form>
   </q-page>
 </template>
@@ -82,9 +106,13 @@ import ConnectWallet from 'components/ConnectWallet';
 import LookupRecipient from 'components/LookupRecipient';
 import helpers from 'src/mixins/helpers';
 
+const umbra = require('umbra-js');
 const addresses = require('../../../addresses.json');
+const umbraAbi = require('../../../contracts/build/contracts/Umbra.json').abi; // eslint-disable-line
+const umbraAddress = require('../../../contracts/.openzeppelin/ropsten.json').proxies['umbra/Umbra'][0].address; // eslint-disable-line
 
-const { utils } = ethers;
+const { constants, utils } = ethers;
+const { KeyPair, RandomNumber } = umbra;
 
 export default {
   name: 'Send',
@@ -98,7 +126,13 @@ export default {
 
   data() {
     return {
+      isSendPending: undefined,
+      isTxProcessing: undefined,
+      txHash: undefined,
+      toll: undefined,
       selectedToken: undefined,
+      selectedTokenAddress: undefined,
+      selectedTokenContract: undefined,
       tokenAmount: undefined,
       tokenList: [
         {
@@ -121,23 +155,42 @@ export default {
     ...mapState({
       userAddress: (state) => state.user.userAddress,
       provider: (state) => state.user.ethersProvider,
+      signer: (state) => state.user.signer,
       recipientPublicKey: (state) => state.user.send.recipientPublicKey,
     }),
 
+    /**
+     * @notice User's balance of the selected token
+     */
     balance() {
       if (!this.balanceBN) return undefined;
       return utils.formatEther(this.balanceBN);
     },
 
+    /**
+     * @notice True if Umbra contract has allowance to spend tokens, false otherwise
+     */
+    hasAllowance() {
+      if (!this.tokenAllowance) return undefined;
+      return this.tokenAllowance.gt(constants.Zero);
+    },
+
+    /**
+     * @notice True if form is correctly filled out, false otherwise
+     */
     isDataValid() {
       const isRecipientValid = !!this.recipientPublicKey;
       const isTokenValid = !!this.selectedToken;
-      const isAmountValid = parseFloat(this.balance) >= parseFloat(this.tokenAmount);
+      const isAmountValid = parseFloat(this.balance) >= 0
+        && parseFloat(this.balance) >= parseFloat(this.tokenAmount);
       return isRecipientValid && isTokenValid && isAmountValid;
     },
   },
 
   asyncComputed: {
+    /**
+     * @notice Returns users balance of the selected currency as BugNumber
+     */
     balanceBN() {
       // Get balance of the selected currency
       if (!this.selectedToken) return undefined;
@@ -145,9 +198,27 @@ export default {
         return this.provider.getBalance(this.userAddress);
       }
       const abi = require(`../../../abi/${this.selectedToken}.json`); // eslint-disable-line
-      const address = addresses[this.selectedToken];
-      const contract = new ethers.Contract(address, abi, this.provider);
-      return contract.balanceOf(this.userAddress);
+      this.selectedTokenAddress = addresses[this.selectedToken];
+      const tokenContract = new ethers.Contract(this.selectedTokenAddress, abi, this.signer);
+      this.selectedTokenContract = tokenContract;
+      return tokenContract.balanceOf(this.userAddress);
+    },
+
+    /**
+     * @notice Returns allowance of Umbra contract to spend selected token
+     */
+    tokenAllowance() {
+      if (!this.selectedTokenContract) return undefined;
+      return this.selectedTokenContract.allowance(this.userAddress, umbraAddress);
+    },
+
+    /**
+     * @notice Current toll
+     */
+    toll() {
+      if (!this.provider) return undefined;
+      const Umbra = new ethers.Contract(umbraAddress, umbraAbi, this.provider);
+      return Umbra.toll();
     },
   },
 
@@ -156,20 +227,135 @@ export default {
       if (!this.selectedToken) return true;
       const balance = parseFloat(this.balance);
       const message = `Please enter a valid amount. You have ${balance} ${this.selectedToken}.`;
-      return balance >= parseFloat(val) ? true : message;
+      return parseFloat(val) > 0 && balance >= parseFloat(val) ? true : message;
     },
 
+    /**
+     * @notice Check if Umbra has approval to spend the token. If not, get approval
+     * then send transaction
+     */
+    async sendFundsFlow() {
+      if (!this.hasAllowance) {
+        await this.getTokenApproval();
+      }
+      await this.sendFunds();
+    },
+
+    /**
+     * @notice Get approval for Umbra to spend tokens
+     */
+    async getTokenApproval() {
+      /* eslint-disable no-console */
+      try {
+        this.isSendPending = true;
+        console.log('Requesting token approval...');
+        const tx = await this.selectedTokenContract.approve(umbraAddress, constants.MaxUint256);
+        this.txHash = tx.hash;
+        this.isTxProcessing = true;
+        console.log('Approval transaction hash: ', this.txHash);
+        console.log('Pending confirmation...');
+        await tx.wait();
+        console.log('Approval transaction complete!');
+        this.txHash = undefined;
+        this.isTxProcessing = false;
+      } catch (err) {
+        this.showError(err);
+      }
+      /* eslint-enable no-console */
+    },
+
+    /**
+     * @notice Send funds via the Umbra contract
+     */
     async sendFunds() {
-      // Create contract instance
+      /* eslint-disable no-console */
+      try {
+        this.isSendPending = true;
+        // Create contract instance
+        const Umbra = new ethers.Contract(umbraAddress, umbraAbi, this.signer);
 
-      // Send transaction
+        // Generate random number and encrypt with recipient's public key
+        const randomNumber = new RandomNumber();
+        const recipientKeyPair = new KeyPair(this.recipientPublicKey);
+        const encrypted = await recipientKeyPair.encrypt(randomNumber);
+        const {
+          iv, ephemeralPublicKey, ciphertext, mac,
+        } = encrypted;
 
-      // Notify user on success
-      this.notifyUser('positive', 'Your payment was successfully sent!');
-      this.tokenAmount = undefined;
-      this.selectedToken = undefined;
-      this.$refs.recipientInput.resetIdentifier();
-      this.$refs.sendForm.resetValidation();
+        // Get x,y coordinates of ephemeral private key
+        const ephemeralPublicKeyCoords = (new KeyPair(ephemeralPublicKey)).publicKeyHexCoords;
+
+        // Break cipher text into three 32-byte components
+        const ciphertextSlim = ciphertext.slice(2); // strip 0x prefix
+        const ciphertextParts = {
+          part1: `0x${ciphertextSlim.slice(0, 64)}`,
+          part2: `0x${ciphertextSlim.slice(64, 128)}`,
+          part3: `0x${ciphertextSlim.slice(128)}`,
+        };
+
+        // Compute stealth address
+        const stealthKeyPair = recipientKeyPair.mulPublicKey(randomNumber);
+        const stealthAddress = stealthKeyPair.address;
+
+        // Send transaction
+        console.log('Sending payment transaction...');
+        let tx;
+        if (this.selectedToken === 'ETH') {
+          const paymentAmount = ethers.BigNumber.from(utils.parseEther(String(this.tokenAmount)));
+          const totalAmount = paymentAmount.add(this.toll);
+          const overrides = { value: totalAmount };
+          tx = await Umbra.sendEth(
+            stealthAddress,
+            iv,
+            ephemeralPublicKeyCoords.x,
+            ephemeralPublicKeyCoords.y,
+            ciphertextParts.part1,
+            ciphertextParts.part2,
+            ciphertextParts.part3,
+            mac,
+            overrides,
+          );
+        } else {
+          // TODO update decimal parsing as more tokens are supported
+          const paymentAmount = utils.parseEther(String(this.tokenAmount));
+          const overrides = { value: this.toll };
+          tx = await Umbra.sendToken(
+            stealthAddress,
+            this.selectedTokenAddress,
+            paymentAmount,
+            iv,
+            ephemeralPublicKeyCoords.x,
+            ephemeralPublicKeyCoords.y,
+            ciphertextParts.part1,
+            ciphertextParts.part2,
+            ciphertextParts.part3,
+            mac,
+            overrides,
+          );
+        }
+        this.txHash = tx.hash;
+        this.isTxProcessing = true;
+        console.log('Transaction hash: ', this.txHash);
+        console.log('Pending confirmation...');
+        await tx.wait();
+        console.log('Transaction complete!');
+
+        // Notify user on success
+        this.notifyUser('positive', 'Your payment was successfully sent!');
+        this.tokenAmount = undefined;
+        this.selectedToken = undefined;
+        this.selectedTokenAddress = undefined;
+        this.$refs.recipientInput.resetIdentifier();
+        this.$refs.sendForm.resetValidation();
+
+        this.isTxProcessing = false;
+        this.isSendPending = false;
+      } catch (err) {
+        this.showError(err);
+        this.isTxProcessing = false;
+        this.isSendPending = false;
+      }
+      /* eslint-enable no-console */
     },
   },
 };
