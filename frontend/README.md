f6e893a22ebfc64c3e28722e12aa61fdf3abc82e
Matt Solomon <matt@mattsolomon.dev>
2020-08-28 15:24:40 -0700

    Begin frontend refactor: setup scaffolding, fonts, and colors

---
+++ frontend/README.md
@@ -0,0 +1,8 @@
+# Umbra App
+
+Send and receive stealth payments with the Umbra protocol.
+
+## Development
+
+1. Install dependencies with `yarn`
+2. Build for development with `yarn run dev`
---
+++ frontend/src/router/index.ts
@@ -0,0 +1,25 @@
+import { route } from 'quasar/wrappers';
+import VueRouter from 'vue-router';
+import routes from './routes';
+
+/*
+ * If not building with SSR mode, you can
+ * directly export the Router instantiation
+ */
+
+export default route(function ({ Vue }) {
+  Vue.use(VueRouter);
+
+  const Router = new VueRouter({
+    scrollBehavior: () => ({ x: 0, y: 0 }),
+    routes,
+
+    // Leave these as is and change from quasar.conf.js instead!
+    // quasar.conf.js -> build -> vueRouterMode
+    // quasar.conf.js -> build -> publicPath
+    mode: process.env.VUE_ROUTER_MODE,
+    base: process.env.VUE_ROUTER_BASE
+  });
+
+  return Router;
+})
