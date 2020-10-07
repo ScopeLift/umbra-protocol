88ccb8725f42004a35995bb74a0ffa47b900894f
Matt Solomon <matt@mattsolomon.dev>
2020-10-07 13:47:32 -0700

    Update connect wallet approach

---
+++ frontend/src/components/HomeActionCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <connect-wallet :to="to">
+    <q-card class="card-border action-card">
+      <q-card-section class="text-h6 header-black card-header"> {{ text }} </q-card-section>
+    </q-card>
+  </connect-wallet>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+import ConnectWallet from 'components/ConnectWallet.vue';
+
+export default defineComponent({
+  name: 'HomeActionCard',
+
+  components: { ConnectWallet },
+
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+
+    to: {
+      type: String,
+      required: true,
+    },
+  },
+
+  setup() {
+    return {};
+  },
+});
+</script>
--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -4,14 +4,20 @@
       <h2 class="header-black">Send Stealth Payments</h2>
       <h4 class="darkgrey q-mt-md">Only the sender and recipient know who received funds</h4>
     </div>
+
+    <div class="row justify-center">
+      <home-action-card to="send" text="Send" />
+    </div>
   </q-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api';
+import HomeActionCard from 'components/HomeActionCard.vue';
 
 export default defineComponent({
   name: 'PageHome',
+  components: { HomeActionCard },
   setup() {
     return {};
   },
