35af09df631f0fbe55f6689eb04b3924cf336877
Matt Solomon <matt@mattsolomon.dev>
2020-10-07 15:05:26 -0700

    Setup receive page

---
+++ frontend/src/pages/AccountReceive.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page padding>
+    <h1 class="page-title">Receive</h1>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'PageReceive',
+  setup() {
+    return {};
+  },
+});
+</script>
