c9a68b0489ea69a9986a12e2334abd0e85f1c57d
Matt Solomon <matt@mattsolomon.dev>
2020-11-24 07:28:39 -0800

    Create Umbra class and test file

---
+++ umbra-js/test/Umbra.test.ts
@@ -0,0 +1,20 @@
+import { Umbra } from '../src/classes/Umbra';
+import chai from 'chai';
+import { provider } from '@openzeppelin/test-environment';
+import type { ExternalProvider } from '../src/types';
+
+const { expect } = chai;
+const web3Provider = (provider as unknown) as ExternalProvider;
+
+describe.only('Umbra class', () => {
+  let umbra: Umbra;
+
+  beforeEach(async () => {
+    umbra = await Umbra.create(web3Provider);
+    expect(true).to.be.true;
+  });
+
+  it('initializes correctly', () => {
+    console.log(umbra);
+  });
+});
