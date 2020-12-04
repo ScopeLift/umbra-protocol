e0945262d02c584900f0ad7335222aa4fd5e2853
wildmolasses <changes@gmail.com>
2020-12-03 19:39:35 -0600

    migrate index.js to src/index.ts

---
+++ umbra-js/src/index.ts
@@ -0,0 +1,17 @@
+import { KeyPair } from './classes/KeyPair';
+import { RandomNumber } from './classes/RandomNumber';
+import { DomainService } from './classes/DomainService';
+import * as ens from './utils/ens';
+import * as cns from './utils/cns';
+import * as utils from './utils/utils';
+import * as constants from './constants.json';
+
+export default {
+  KeyPair,
+  RandomNumber,
+  DomainService,
+  ens,
+  cns,
+  utils,
+  constants,
+};
