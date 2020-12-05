f259e30bb12be1b1630b15e3aea3bcb1c34b03ca
wildmolasses <changes@gmail.com>
2020-12-05 15:04:42 -0600

    small typescript changes

--- frontend/src/components/models.ts
+++ frontend/src/components/models.ts
@@ -1,9 +1,7 @@
 import { ethers } from 'ethers';
-import { KeyPair } from '@umbra/umbra-js';
 
 export type Signer = ethers.providers.JsonRpcSigner;
 export type Provider = ethers.providers.Web3Provider;
 export type Network = ethers.providers.Network;
 export type BigNumber = ethers.BigNumber;
 export type TransactionResponse = ethers.providers.TransactionResponse;
-export type KeyPair = typeof KeyPair;
