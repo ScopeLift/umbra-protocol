9caa4c6b0251a9056db330198f003404682d969c
Matt Solomon <matt@mattsolomon.dev>
2021-01-04 16:16:33 -0800

    Scaffold methods for interacting with Umbra

--- umbra-js/test/Umbra.test.ts
+++ umbra-js/test/Umbra.test.ts
@@ -5,16 +5,24 @@ import type { ExternalProvider } from '../src/types';
 
 const { expect } = chai;
 const web3Provider = (provider as unknown) as ExternalProvider;
+const INFURA_ID = process.env.INFURA_ID;
+const JSON_RPC_URL = `https://ropsten.infura.io/v3/${INFURA_ID}`;
+
+const createUmbraInstance = async () => await Umbra.create(web3Provider);
+const createReadonlyUmbraInstance = async () => await Umbra.createReadonly(JSON_RPC_URL);
 
 describe.only('Umbra class', () => {
-  let umbra: Umbra;
+  describe('Initialization', () => {
+    it('initializes correctly when using a web3 provider', async () => {
+      const umbra = await createUmbraInstance();
+      expect(umbra.provider._isProvider).to.be.true;
+      expect(umbra.signer._isSigner).to.be.true;
+    });
 
-  beforeEach(async () => {
-    umbra = await Umbra.create(web3Provider);
-    expect(true).to.be.true;
-  });
-
-  it('initializes correctly', () => {
-    console.log(umbra);
+    it('initializes correctly when using a JSON-RPC provider', async () => {
+      const umbra = await createReadonlyUmbraInstance();
+      expect(umbra.provider._isProvider).to.be.true;
+      expect(umbra.signer._isSigner).to.be.true;
+    });
   });
 });
