6cc1afb0c43fa360f86a51b8edafd3dd6e65c6ad
Matt Solomon <matt@mattsolomon.dev>
2021-01-08 09:31:22 -0800

    Update class interface + tests for more flexibility on which signer to use

--- umbra-js/src/classes/Umbra.ts
+++ umbra-js/src/classes/Umbra.ts
@@ -4,10 +4,17 @@
 
 import { KeyPair } from './KeyPair';
 import { Contract } from 'ethers';
-import { Umbra as UmbraContract } from '../../types/contracts';
+import { Umbra as UmbraContract, ERC20 } from '../../types/contracts';
 import { getAddress } from '@ethersproject/address';
+import { BigNumber } from '@ethersproject/bignumber';
 import { JsonRpcProvider, JsonRpcSigner, Web3Provider } from '@ethersproject/providers';
-import type { ExternalProvider, UmbraOverrides } from '../types';
+import * as erc20Abi from '../abi/ERC20.json';
+import type {
+  // ExternalProvider,
+  UmbraOverrides,
+  // Announcement,
+  // UserAnnouncementEvent,
+} from '../types';
 
 // Umbra.sol ABI
 const abi = require('../../../contracts/build/contracts/Umbra.json').abi;
@@ -35,9 +42,12 @@ const isETH = (token: string) => {
   return getAddress(token) === ETH_ADDRESS; // throws if not a valid address
 };
 
+// Error message to throw if a valid signer is required but not present
+const missingSignerMessage =
+  'This method requires a valid signer, but Umbra instance has a readonly provider';
+
 export class Umbra {
-  readonly ethersProvider: Web3Provider | JsonRpcProvider;
-  readonly signer: JsonRpcSigner;
+  readonly signer: JsonRpcSigner | null; // null if connected with a readonly provider
   readonly umbra: UmbraContract;
   readonly chainId: number;
 
@@ -49,43 +59,44 @@ export class Umbra {
    * certain that the provided chain ID matches the provider's chain ID
    * @param provider ethers provider to use
    * @param chainId The Chain ID of the network
+   * @param signer The associated signer, or null if no signer is available
    */
-  constructor(readonly provider: Web3Provider | JsonRpcProvider, chainId: number) {
+  constructor(
+    readonly provider: Web3Provider | JsonRpcProvider,
+    chainId: number,
+    signer: JsonRpcSigner | null
+  ) {
     const supportedChainIds = Object.keys(contractInfo);
     if (!supportedChainIds.includes(String(chainId))) {
       throw new Error('Unsupported chainId');
     }
 
+    this.signer = signer;
     this.chainId = chainId;
-    this.ethersProvider = provider;
-    this.signer = this.ethersProvider.getSigner();
     this.umbra = new Contract(
       contractInfo[chainId].umbraAddress,
       abi,
-      this.signer || this.ethersProvider
+      this.signer || this.provider
     ) as UmbraContract;
   }
 
   /**
    * @notice Generates Umbra instance from a web3 provider to read from and write to the Umbra contracts
-   * @param provider web3 provider to use (not an ethers provider)
+   * @param signer Signer to use
    */
-  static async create(provider: ExternalProvider) {
-    const ethersProvider = new Web3Provider(provider);
-    const network = await ethersProvider.getNetwork();
-    const chainId = network.chainId;
-    return new Umbra(ethersProvider, chainId);
+  static async create(signer: JsonRpcSigner) {
+    const chainId = await signer.getChainId();
+    return new Umbra(signer.provider, chainId, signer);
   }
 
   /**
    * @notice Generates Umbra instance from a JSON-RPC API to read from the Umbra contracts
-   * @param provider web3 provider to use (not an ethers provider)
+   * @param url URL of the JSON-RPC provider to connect to
    */
   static async createReadonly(url: string) {
-    const ethersProvider = new JsonRpcProvider(url);
-    const network = await ethersProvider.getNetwork();
-    const chainId = network.chainId;
-    return new Umbra(ethersProvider, chainId);
+    const provider = new JsonRpcProvider(url);
+    const network = await provider.getNetwork();
+    return new Umbra(provider, network.chainId, null);
   }
 
   // ==================================== CONTRACT INTERACTION =====================================
@@ -95,27 +106,47 @@ export class Umbra {
    * @dev If sending tokens, make sure to handle the approvals before calling this method
    * @param token Address of token to send. Use 'ETH' or '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'
    * to send Ether
+   * @param amount Amount to send, in units of that token (e.g. use 1e6 to send 1 USDC)
    * @param recipientId Identifier of recipient, e.g. their ENS name
    * @param overrides Override the payload extension, gas limit, gas price, or nonce
    */
-  async send(token: string, recipientId: string, overrides: UmbraOverrides = {}) {
+  async send(
+    token: string,
+    amount: BigNumber | string,
+    recipientId: string,
+    overrides: UmbraOverrides = {}
+  ) {
+    // Check for valid signer
+    if (!this.signer) throw new Error(missingSignerMessage);
+
     // Check that sender has sufficient balance
-    // TODO
-
-    // Generate random number and encrypt with recipient's public key
-    // TODO
-
-    // Get x,y coordinates of ephemeral private key
-    // TODO
-
-    // Compute stealth address
-
-    // Send funds
-    if (isETH(token)) {
-      // await this.umbra.sendEth(...);
-    } else {
-      // await this.umbra.sendToken(...);
+    const tokenContract = new Contract(token, erc20Abi, this.signer) as ERC20;
+    console.log('await this.signer.getAddress(): ', await this.signer.getAddress());
+    const tokenBalance = await tokenContract.balanceOf(await this.signer.getAddress());
+    console.log('tokenBalance: ', tokenBalance);
+    if (tokenBalance.lt(amount)) {
+      const providedAmount = BigNumber.from(amount).toString();
+      const details = `Has ${tokenBalance.toString()} tokens, tried to send ${providedAmount} tokens.`;
+      throw new Error(`Insufficient balance to complete transfer. ${details}`);
     }
+
+    console.log(token, amount, recipientId, overrides);
+    console.log('isETH(token): ', isETH(token));
+
+    // //
+    // if (isETH(token)) {
+    //   // Generate random number and encrypt with recipient's public key
+    //   // TODO
+    //   // Get x,y coordinates of ephemeral private key
+    //   // TODO
+    //   // Compute stealth address
+    //   // Send funds
+    //   // await this.umbra.sendEth(...);
+    // } else {
+    //   // await this.umbra.sendToken(...);
+    // }
+
+    return 0;
   }
 
   /**
@@ -123,17 +154,19 @@ export class Umbra {
    * @param recipient Recipient address
    * @param overrides Override the payload extension, gas limit, gas price, or nonce
    */
-  async withdraw(token: string, recipient: string, overrides: UmbraOverrides = {}) {
+  withdraw(token: string, recipient: string, overrides: UmbraOverrides = {}) {
+    console.log(token, recipient, overrides);
+
     // Determine if recipient caller is withdrawing token or ETH by reading contract
     // TODO
 
-    // Withdraw funds
-    if (isETH(token)) {
-      // Based on gas price, compute how much ETH to transfer to avoid dust
-      // TODO
-    } else {
-      // this.umbra.withdrawToken(...);
-    }
+    // // Withdraw funds
+    // if (isETH(token)) {
+    //   // Based on gas price, compute how much ETH to transfer to avoid dust
+    //   // TODO
+    // } else {
+    //   // this.umbra.withdrawToken(...);
+    // }
   }
 
   /**
@@ -142,8 +175,9 @@ export class Umbra {
    */
   async scan(keyPair: KeyPair) {
     // Get start and end blocks to scan events for
+    console.log('keyPair: ', keyPair);
     const startBlock = contractInfo[this.chainId].startBlock;
-    const endBlock = await this.ethersProvider.getBlockNumber();
+    const endBlock = await this.provider.getBlockNumber();
 
     // Get list of all Announcement events
     const announcementFilter = this.umbra.filters.Announcement(null, null, null, null, null); // gets all  announcements
@@ -156,50 +190,52 @@ export class Umbra {
     // Get list of all TokenWithdrawal events
     const withdrawalFilter = this.umbra.filters.TokenWithdrawal(null, null, null, null); // gets all  announcements
     const withdrawalEvents = await this.umbra.queryFilter(withdrawalFilter, startBlock, endBlock);
+    console.log('withdrawalEvents: ', withdrawalEvents);
 
     // Determine which announcements are for the user
-    const userAnnouncementEvents = [];
+    const userAnnouncementEvents = [] as Event[];
     for (let i = 0; i < announcementEvents.length; i += 1) {
       const event = announcementEvents[i];
+      console.log('event.args: ', event.args);
 
       // Extract out event parameters
-      const { receiver, amount, token, pkx, ciphertext } = event.args;
+      // const { receiver, amount, token, pkx, ciphertext } = (event.args as unknown) as Announcement;
 
       // Get y-coordinate of public key from the x-coordinate
       // TODO
 
       // Decrypt to get random number
-      const payload = {
-        ephemeralPublicKey: `0x04${pkx.slice(2)}${pky.slice(2)}`,
-        ciphertext,
-      };
-      const randomNumber = await keyPair.decrypt(payload); // eslint-disable-line
+      // const payload = {
+      //   ephemeralPublicKey: `0x04${pkx.slice(2)}${pky.slice(2)}`,
+      //   ciphertext,
+      // };
+      // const randomNumber = await keyPair.decrypt(payload); // eslint-disable-line
 
-      // Get what our receiving address would be with this random number
-      const computedReceivingAddress = keyPair.mulPublicKey(randomNumber).address;
+      // // Get what our receiving address would be with this random number
+      // const computedReceivingAddress = keyPair.mulPublicKey(randomNumber).address;
 
       // If our receiving address matches the event's recipient, the transfer was for us
-      if (computedReceivingAddress === receiver) {
-        // Get block number, timestamp, and sender of this event
-        // TODO
+      // if (computedReceivingAddress === receiver) {
+      //   // Get block number, timestamp, and sender of this event
+      //   // TODO
 
-        // Save off data
-        userAnnouncementEvents.push({
-          ...event,
-          randomNumber,
-          receiver,
-          amount,
-          token,
-          blockNumber,
-          timestamp,
-          sender,
-        });
-      }
+      //   // Save off data
+      //   userAnnouncementEvents.push({
+      //     event,
+      //     randomNumber,
+      //     receiver,
+      //     amount,
+      //     token,
+      //     blockNumber,
+      //     timestamp,
+      //     sender,
+      //   });
+      // }
     }
 
     // For each of the user's announcement events, determine if the funds were withdrawn
     // TODO
 
-    return { userAnnouncementEvents, userWithdrawalEvents, userEvents };
+    return { userAnnouncementEvents /* userWithdrawalEvents, userEvents */ };
   }
 }
--- umbra-js/test/Umbra.test.ts
+++ umbra-js/test/Umbra.test.ts
@@ -1,28 +1,94 @@
 import { Umbra } from '../src/classes/Umbra';
+import { BigNumberish, ethers } from 'ethers';
 import * as chai from 'chai';
-import { provider } from '@openzeppelin/test-environment';
+import { accounts, contract, provider } from '@openzeppelin/test-environment';
 import type { ExternalProvider } from '../src/types';
+import { TestToken as ERC20 } from '../types/contracts/TestToken';
 
+const TestToken = contract.fromArtifact('TestToken');
 const { expect } = chai;
 const web3Provider = (provider as unknown) as ExternalProvider;
 const INFURA_ID = process.env.INFURA_ID;
 const JSON_RPC_URL = `https://ropsten.infura.io/v3/${INFURA_ID}`;
 
-const createUmbraInstance = async () => await Umbra.create(web3Provider);
-const createReadonlyUmbraInstance = async () => await Umbra.createReadonly(JSON_RPC_URL);
+const [sender, receiver] = accounts;
+const quantity = ethers.utils.parseEther('100');
+
+const mint = async (token: ERC20, to: string, amount: BigNumberish) => await token.mint(to, amount);
+
+/**
+ * @notice Wrapper function to verify that an async function rejects with the specified message
+ * @param promise Promise to wait for
+ * @param message Expected rejection message
+ */
+const expectRejection = async (promise: Promise<any>, message: string) => {
+  // Error type requires strings, so we set them to an arbitrary value and
+  // later check the values. If unchanged, the promise did not reject
+  let error: Error = { name: 'default', message: 'default' };
+  try {
+    await promise;
+  } catch (e) {
+    error = e;
+  } finally {
+    expect(error.name).to.not.equal('default');
+    expect(error.message).to.not.equal('default');
+    expect(error.message).to.equal(message);
+  }
+};
 
 describe.only('Umbra class', () => {
+  let dai: ERC20;
+  let umbra: Umbra;
+  let umbraReadonly: Umbra;
+
+  beforeEach(async () => {
+    // Get signers
+    const ethersProvider = new ethers.providers.Web3Provider(web3Provider);
+    const signerSender = ethersProvider.getSigner(1);
+    // const signerReceiver = ethersProvider.getSigner(2);
+
+    // Get Umbra instances
+    umbra = await Umbra.create(signerSender);
+    umbraReadonly = await Umbra.createReadonly(JSON_RPC_URL);
+
+    // Deploy mock tokens
+    dai = (await TestToken.new('Dai', 'DAI')) as ERC20;
+  });
+
   describe('Initialization', () => {
     it('initializes correctly when using a web3 provider', async () => {
-      const umbra = await createUmbraInstance();
       expect(umbra.provider._isProvider).to.be.true;
-      expect(umbra.signer._isSigner).to.be.true;
+      expect(Boolean(umbra.signer)).to.be.true;
     });
 
     it('initializes correctly when using a JSON-RPC provider', async () => {
-      const umbra = await createReadonlyUmbraInstance();
-      expect(umbra.provider._isProvider).to.be.true;
-      expect(umbra.signer._isSigner).to.be.true;
+      expect(umbraReadonly.provider._isProvider).to.be.true;
+      expect(Boolean(umbraReadonly.signer)).to.be.false;
+    });
+  });
+
+  describe('Sends funds', () => {
+    it.only('reverts if sender does not have enough funds', async () => {
+      const msg = `Insufficient balance to complete transfer. Has 0 tokens, tried to send ${quantity.toString()} tokens.`;
+      await expectRejection(umbra.send(dai.address, quantity, receiver), msg);
+    });
+
+    it('sends tokens without payload extension', async () => {
+      await mint(dai, sender, quantity);
+      const tx = await umbra.send(dai.address, quantity, receiver);
+      console.log(tx);
+    });
+
+    it('sends tokens with payload extension', async () => {
+      //
+    });
+
+    it('sends ETH without payload extension', async () => {
+      //
+    });
+
+    it('sends ETH with payload extension', async () => {
+      //
     });
   });
 });
