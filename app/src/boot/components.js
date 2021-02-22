cdb925a80e3371855a68a1093289690c2af55a96
Matt Solomon <matt@mattsolomon.dev>
2021-02-22 15:04:02 -0800

    Remove old frontend (the 'app' folder)

--- app/src/boot/components.js
+++
@@ -1,8 +0,0 @@
-import BaseButton from 'components/BaseButton';
-import BaseInput from 'components/BaseInput';
-
-// "async" is optional
-export default async ({ Vue /* app, router, store, ... */ }) => {
-  Vue.component('base-button', BaseButton);
-  Vue.component('base-input', BaseInput);
-};
--- app/src/boot/vue-async-computed.js
+++
@@ -1,6 +0,0 @@
-import AsyncComputed from 'vue-async-computed';
-
-// more info on params: https://quasar.dev/quasar-cli/cli-documentation/boot-files#Anatomy-of-a-boot-file
-export default async ({ Vue }) => {
-  Vue.use(AsyncComputed);
-};
