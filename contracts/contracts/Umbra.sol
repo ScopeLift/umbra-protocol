058438843930aeefb8127ab6a503a2128d087e5e
Ben DiFrancesco <ben@scopelift.co>
2021-03-03 22:57:37 -0500

    Add Natspec comments to the core Umbra contract

--- contracts/contracts/Umbra.sol
+++ contracts/contracts/Umbra.sol
@@ -10,27 +10,55 @@ import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 contract Umbra is Ownable {
   using SafeMath for uint256;
 
-  string public constant version = "1";
-  uint256 public immutable chainId;
+  // =========================================== Events ============================================
 
+  /// @notice Emitted when a payment is sent
   event Announcement(
-    address indexed receiver,
-    uint256 indexed amount,
-    address indexed token,
+    address indexed receiver, // stealth address
+    uint256 indexed amount, // funds
+    address indexed token, // token address or ETH placeholder
     bytes32 pkx, // ephemeral public key x coordinate
-    bytes32 ciphertext
+    bytes32 ciphertext // encrypted entropy and payload extension
   );
 
-  event TokenWithdrawal(address indexed receiver, address indexed acceptor, uint256 amount, address indexed token);
+  /// @notice Emitted when a token is withdrawn
+  event TokenWithdrawal(
+    address indexed receiver, // stealth address
+    address indexed acceptor, // destination of funds
+    uint256 amount, // funds
+    address indexed token // token address
+  );
 
+  // ======================================= State variables =======================================
+
+  /// @notice Version string for this Umbra contract
+  string public constant version = "1";
+
+  /// @notice Chain identifier where this contract is deployed; set in constructor
+  uint256 public immutable chainId;
+
+  /// @dev Placeholder address used to identify transfer of native ETH
   address constant ETH_TOKEN_PLACHOLDER = address(0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE);
 
+  /// @notice An ETH amount that must be sent alongside each payment; used as an anti-spam measure
   uint256 public toll;
+
+  /// @notice A privileged address, set by the admin, that can sweep all collected ETH tolls
   address public tollCollector;
+
+  /// @notice The address where ETH funds are sent when collected by the tollCollector
   address payable public tollReceiver;
-  // stealth address => token address => amount
+
+  /// @notice Token payments pending withdrawal; stealth address => token address => amount
   mapping(address => mapping(address => uint256)) public tokenPayments;
 
+  // ======================================= Setup & Admin ========================================
+
+  /**
+   * @param _toll Amount of ETH required per send
+   * @param _tollCollector Address that can sweep collected funds
+   * @param _tollReceiver Address that receives collected funds
+   */
   constructor(
     uint256 _toll,
     address _tollCollector,
@@ -49,18 +77,46 @@ contract Umbra is Ownable {
     chainId = _chainId;
   }
 
+  /**
+   * @notice Admin only function to update the toll
+   * @param _newToll New ETH toll in wei
+   */
   function setToll(uint256 _newToll) public onlyOwner {
     toll = _newToll;
   }
 
+  /**
+   * @notice Admin only function to update the toll collector
+   * @param _newTollCollector New address which has fund sweeping privileges
+   */
   function setTollCollector(address _newTollCollector) public onlyOwner {
     tollCollector = _newTollCollector;
   }
 
+  /**
+   * @notice Admin only function to update the toll receiver
+   * @param _newTollReceiver New address which receives collected funds
+   */
   function setTollReceiver(address payable _newTollReceiver) public onlyOwner {
     tollReceiver = _newTollReceiver;
   }
 
+  /**
+   * @notice Function only the toll collector can call to sweep funds to the toll receiver
+   */
+  function collectTolls() public onlyCollector {
+    tollReceiver.transfer(address(this).balance);
+  }
+
+  // ======================================= Send =================================================
+
+  /**
+   * @notice Send and announce ETH payment to a stealth address
+   * @param _receiver Stealth address receiving the payment
+   * @param _tollCommitment Exact toll the sender is paying; should equal contract toll
+   * @param _pkx X-coordinate of the ephemeral public key used to encrypt the payload
+   * @param _ciphertext Encrypted entropy (used to generated the stealth address) and payload extension
+   */
   function sendEth(
     address payable _receiver,
     uint256 _tollCommitment,
@@ -76,6 +132,14 @@ contract Umbra is Ownable {
     _receiver.transfer(amount);
   }
 
+  /**
+   * @notice Send and announce an ERC20 payment to a stealth address
+   * @param _receiver Stealth address receiving the payment
+   * @param _tokenAddr Address of the ERC20 token being sent
+   * @param _amount Amount of the token to send, in its own base units
+   * @param _pkx X-coordinate of the ephemeral public key used to encrypt the payload
+   * @param _ciphertext Encrypted entropy (used to generated the stealth address) and payload extension
+   */
   function sendToken(
     address _receiver,
     address _tokenAddr,
@@ -92,6 +156,13 @@ contract Umbra is Ownable {
     SafeERC20.safeTransferFrom(IERC20(_tokenAddr), msg.sender, address(this), _amount);
   }
 
+  // ======================================= Withdraw =============================================
+
+  /**
+   * @notice Withdraw an ERC20 token payment sent to a stealth address, which must be the sender
+   * @param _acceptor Address where withdrawn funds should be sent
+   * @param _tokenAddr Address of the ERC20 token being withdrawn
+   */
   function withdrawToken(address _acceptor, address _tokenAddr) public {
     uint256 amount = tokenPayments[msg.sender][_tokenAddr];
 
@@ -103,6 +174,17 @@ contract Umbra is Ownable {
     SafeERC20.safeTransfer(IERC20(_tokenAddr), _acceptor, amount);
   }
 
+  /**
+   * @notice Withdraw an ERC20 token payment on behalf of a stealth address via signed authorization
+   * @param _stealthAddr The stealth address whose token balance will be withdrawn
+   * @param _acceptor Address where withdrawn funds should be sent
+   * @param _tokenAddr Address of the ERC20 token being withdrawn
+   * @param _sponsor Address which is compensated for submitting the withdrawal tx
+   * @param _sponsorFee Amount of the token to pay to the transfer
+   * @param _v ECSDA signature prefix
+   * @param _r ECSDA signature x-coordinate
+   * @param _s ECSDA signature y-coordinate
+   */
   function withdrawTokenOnBehalf(
     address _stealthAddr,
     address _acceptor,
@@ -146,9 +228,7 @@ contract Umbra is Ownable {
     SafeERC20.safeTransfer(IERC20(_tokenAddr), _sponsor, _sponsorFee);
   }
 
-  function collectTolls() public onlyCollector {
-    tollReceiver.transfer(address(this).balance);
-  }
+  // ======================================= Modifiers ============================================
 
   modifier onlyCollector() {
     require(msg.sender == tollCollector, "Umbra: Not Toll Collector");
