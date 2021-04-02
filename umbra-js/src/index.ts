f8f602811308bda3d0e813b5d45e55abcedc3950
Matt Solomon <matt@mattsolomon.dev>
2021-04-02 14:54:35 -0700

    Remove unused constants.json file

--- umbra-js/src/index.ts
+++ umbra-js/src/index.ts
@@ -2,10 +2,9 @@ import { DomainService } from './classes/DomainService';
 import { KeyPair } from './classes/KeyPair';
 import { RandomNumber } from './classes/RandomNumber';
 import { Umbra } from './classes/Umbra';
-import * as constants from './constants.json';
 import * as cns from './utils/cns';
 import * as ens from './utils/ens';
 import * as utils from './utils/utils';
 
 export { ChainConfig, SendOverrides, ScanOverrides, Announcement, UserAnnouncement } from './types';
-export { KeyPair, RandomNumber, DomainService, Umbra, ens, cns, utils, constants };
+export { KeyPair, RandomNumber, DomainService, Umbra, ens, cns, utils };
