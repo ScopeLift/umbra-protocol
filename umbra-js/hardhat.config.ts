66a22b66eda8c63e1f7732a1ffb1f839fe7cc315
Ed Mazurek <Edward.R.Mazurek@gmail.com>
2021-04-09 10:50:32 -0500

    PR Feedback
    Co-authored-by: Matt <matt@mattsolomon.dev>

--- umbra-js/hardhat.config.ts
+++ umbra-js/hardhat.config.ts
@@ -4,16 +4,9 @@ dotenvConfig({ path: resolve(__dirname, './.env') });
 
 import { HardhatUserConfig } from 'hardhat/config';
 import { NetworkUserConfig } from 'hardhat/types';
-import './tasks/accounts';
-import './tasks/clean';
 
-import '@nomiclabs/hardhat-waffle';
-import '@nomiclabs/hardhat-web3';
-import '@nomiclabs/hardhat-truffle5';
 import '@nomiclabs/hardhat-ethers';
-import 'hardhat-typechain';
-import 'solidity-coverage';
-import 'hardhat-gas-reporter';
+import '@nomiclabs/hardhat-waffle';
 
 const chainIds = {
   ganache: 1337,
@@ -26,11 +19,10 @@ const chainIds = {
 };
 
 // Ensure that we have all the environment variables we need.
+const mnemonic = 'test test test test test test test test test test test junk';
+
 const infuraApiKey = process.env.INFURA_API_KEY;
 if (!infuraApiKey) throw new Error('Please set your INFURA_API_KEY in a .env file');
-
-const mnemonic = 'test test test test test test test test test test test junk';
-const shouldReportGas = process.env.REPORT_GAS === 'true';
 
 function createTestnetConfig(network: keyof typeof chainIds): NetworkUserConfig {
   const url = `https://${network}.infura.io/v3/${infuraApiKey as string}`;
@@ -67,9 +59,7 @@ const config: HardhatUserConfig = {
     ropsten: createTestnetConfig('ropsten'),
   },
   paths: {
-    artifacts: './artifacts',
     cache: './cache',
-    sources: './contracts',
     tests: './test',
   },
   solidity: {
@@ -95,16 +85,6 @@ const config: HardhatUserConfig = {
         },
       },
     ],
-  },
-  typechain: {
-    outDir: 'typechain',
-    target: 'ethers-v5',
-  },
-  gasReporter: {
-    enabled: shouldReportGas,
-    currency: 'USD',
-    gasPrice: 200,
-    excludeContracts: ['TestToken.sol', 'MockHook.sol', 'ERC20.sol', 'open_gsn/'],
   },
 };
 
