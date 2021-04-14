7a7c5f22aaebcad991e89438babe2e0884734ca0
Matt Solomon <matt@mattsolomon.dev>
2021-04-14 12:44:38 -0700

    FAQ page setup

---
+++ frontend/src/components/FAQItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-expansion-item class="q-py-sm" group="faq" header-style="font-size: 1.1em;" :label="question">
+    <q-card>
+      <q-card-section>
+        <slot></slot>
+      </q-card-section>
+    </q-card>
+  </q-expansion-item>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'FAQItem',
+
+  props: {
+    question: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
