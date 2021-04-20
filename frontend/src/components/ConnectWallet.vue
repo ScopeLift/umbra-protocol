0d2ccd46192abc09275a519d89fac3286c114588
Matt Solomon <matt@mattsolomon.dev>
2021-04-20 15:42:07 -0700

    Reduce set of supported wallets
    Wallets must use deterministic signatures to be safely supported, so we
    reduce the set of supported wallets to only those zkSync uses (they use
    a similar signature approach for generating  private keys

--- frontend/src/components/ConnectWallet.vue
+++ frontend/src/components/ConnectWallet.vue
@@ -24,24 +24,13 @@ function useWallet(context: SetupContext, to: string) {
     const rpcUrl = `https://mainnet.infura.io/v3/${String(process.env.INFURA_ID)}`;
     const wallets = [
       { walletName: 'metamask', preferred: true },
-      { walletName: 'coinbase', preferred: true },
-      { walletName: 'torus', preferred: true },
-      { walletName: 'ledger', rpcUrl },
-      { walletName: 'trezor', appUrl: 'https://umbra.cash/', email: 'matt@scopelift.co', rpcUrl },
-      { walletName: 'lattice', rpcUrl, appName: 'Umbra' },
+      { walletName: 'walletConnect', infuraKey: process.env.INFURA_ID, preferred: true },
       { walletName: 'fortmatic', apiKey: process.env.FORTMATIC_API_KEY, preferred: true },
       { walletName: 'portis', apiKey: process.env.PORTIS_API_KEY },
-      { walletName: 'authereum' },
-      { walletName: 'walletConnect', infuraKey: process.env.INFURA_ID, preferred: true },
-      { walletName: 'trust', rpcUrl },
-      { walletName: 'walletLink', rpcUrl, label: 'Coinbase Wallet (WalletLink)' },
+      { walletName: 'ledger', rpcUrl },
+      { walletName: 'torus', preferred: true },
+      { walletName: 'lattice', rpcUrl, appName: 'Umbra' },
       { walletName: 'opera' },
-      { walletName: 'operaTouch' },
-      { walletName: 'status' },
-      { walletName: 'imToken', rpcUrl },
-      { walletName: 'meetone' },
-      { walletName: 'mykey', rpcUrl },
-      { walletName: 'huobiwallet', rpcUrl },
     ];
     const walletChecks = [{ checkName: 'connect' }, { checkName: 'network' }];
 
