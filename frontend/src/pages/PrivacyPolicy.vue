a1a59849ddc897baafd16c54761143e7690b9789
Matt Solomon <matt@mattsolomon.dev>
2021-04-20 09:18:58 -0700

    Add Privacy Policy

--- frontend/src/pages/PrivacyPolicy.vue
+++ frontend/src/pages/PrivacyPolicy.vue
@@ -2,7 +2,87 @@
   <q-page padding class="text-left form-extra-wide">
     <h2 class="text-center page-title">Privacy Policy</h2>
 
-    TODO
+    <p class="text-bold">What information do we collect?</p>
+    <p>
+      When using our site, we may record the IP address that the post originated from. We also may retain server logs
+      which include the IP address of every request to our server.
+    </p>
+
+    <p class="text-bold">What do we use your information for?</p>
+    <p>Any of the information we collect from you may be used in one of the following ways:</p>
+    <ul>
+      <li>To personalize your experience — your information helps us to better respond to your individual needs.</li>
+      <li>
+        To improve our site — we continually strive to improve our site offerings based on the information and feedback
+        we receive from you.
+      </li>
+      <li>
+        To improve customer service — your information helps us to more effectively respond to your customer service
+        requests and support needs.
+      </li>
+    </ul>
+
+    <p class="text-bold">How do we protect your information?</p>
+    <p>
+      We implement a variety of security measures to maintain the safety of your personal information when you enter,
+      submit, or access your personal information.
+    </p>
+
+    <p class="text-bold">What is your data retention policy?</p>
+    <p>We will make a good faith effort to:</p>
+    <ul>
+      <li>Retain server logs containing the IP address of all requests to this server no more than 90 days.</li>
+      <li>Retain the IP addresses associated with registered users and their posts no more than 5 years.</li>
+    </ul>
+
+    <p class="text-bold">Do we use cookies?</p>
+    <p>
+      We do not use cookies. We do use local browser storage to store user preferences and settings between sessions.
+      This data is never used to track users, and not sent anywhere beyond the user’s local device.
+    </p>
+
+    <p class="text-bold">Do we disclose any information to outside parties?</p>
+    <p>
+      We do not sell, trade, or otherwise transfer to outside parties your personally identifiable information. This
+      does not include trusted third parties who assist us in operating our site, conducting our business, or servicing
+      you, so long as those parties agree to keep this information confidential. We may also release your information
+      when we believe release is appropriate to comply with the law, enforce our site policies, or protect ours or
+      others rights, property, or safety. However, non-personally identifiable visitor information may be provided to
+      other parties for marketing, advertising, or other uses.
+    </p>
+
+    <p class="text-bold">Third party links</p>
+    <p>
+      Occasionally, at our discretion, we may include or offer third party products or services on our site. These third
+      party sites have separate and independent privacy policies. We therefore have no responsibility or liability for
+      the content and activities of these linked sites. Nonetheless, we seek to protect the integrity of our site and
+      welcome any feedback about these sites.
+    </p>
+
+    <p class="text-bold">Children’s Online Privacy Protection Act Compliance</p>
+    <p>
+      Our site, products and services are all directed to people who are at least 13 years old or older. If this server
+      is in the USA, and you are under the age of 13, per the requirements of COPPA (<a
+        class="hyperlink"
+        href="https://en.wikipedia.org/wiki/Children%27s_Online_Privacy_Protection_Act"
+        target="_blank"
+        >Children’s Online Privacy Protection Act</a
+      >), do not use this site.
+    </p>
+
+    <p class="text-bold">Online Privacy Policy Only</p>
+    <p>
+      This online privacy policy applies only to information collected through our site and not to information collected
+      offline.
+    </p>
+
+    <p class="text-bold">Your Consent</p>
+    <p>By using our site, you consent to our web site privacy policy.</p>
+
+    <p class="text-bold">Changes to our Privacy Policy</p>
+    <p>If we decide to change our privacy policy, we will post those changes on this page.</p>
+
+    <p>This document is CC-BY-SA. It was last updated April 20, 2021.</p>
   </q-page>
 </template>
 
