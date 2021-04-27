98091fafd0909b599fafb435b68c57c3fd1da8dc
Ben DiFrancesco <ben@scopelift.co>
2021-04-27 11:59:06 -0400

    Make GSN contract deployment configurable via env flag

--- contracts/scripts/deploy.js
+++ contracts/scripts/deploy.js
@@ -13,6 +13,7 @@ const { exit } = require('process');
 const { ethers } = hre;
 
 const network = process.env.HARDHAT_NETWORK;
+const shouldDeployGSN = process.env.DEPLOY_GSN === 'true';
 
 // Initialize object that will hold all deploy info. We'll continually update this and save it to
 // a file using the save() method below
@@ -72,57 +73,59 @@ const save = (value, field, subfield = undefined) => {
     save(umbra.address, 'contracts', 'Umbra');
     console.log('Umbra contract deployed to address: ', umbra.address);
 
-    const UmbraForwarder = await ethers.getContractFactory('UmbraForwarder', adminWallet);
-    const umbraForwarder = await UmbraForwarder.deploy();
-    await umbraForwarder.deployed();
-    save(umbraForwarder.address, 'contracts', 'UmbraForwarder');
-    console.log('UmbraForwarder contract deployed to address: ', umbraForwarder.address);
+    if (shouldDeployGSN) {
+      const UmbraForwarder = await ethers.getContractFactory('UmbraForwarder', adminWallet);
+      const umbraForwarder = await UmbraForwarder.deploy();
+      await umbraForwarder.deployed();
+      save(umbraForwarder.address, 'contracts', 'UmbraForwarder');
+      console.log('UmbraForwarder contract deployed to address: ', umbraForwarder.address);
 
-    const UmbraRelayRecipient = await ethers.getContractFactory('UmbraRelayRecipient', adminWallet);
-    const umbraRelayRecipient = await UmbraRelayRecipient.deploy(umbra.address, umbraForwarder.address);
-    await umbraRelayRecipient.deployed();
-    save(umbraRelayRecipient.address, 'contracts', 'UmbraRelayRecipient');
-    console.log('UmbraRelayRecipient contract deployed to address: ', umbraRelayRecipient.address);
+      const UmbraRelayRecipient = await ethers.getContractFactory('UmbraRelayRecipient', adminWallet);
+      const umbraRelayRecipient = await UmbraRelayRecipient.deploy(umbra.address, umbraForwarder.address);
+      await umbraRelayRecipient.deployed();
+      save(umbraRelayRecipient.address, 'contracts', 'UmbraRelayRecipient');
+      console.log('UmbraRelayRecipient contract deployed to address: ', umbraRelayRecipient.address);
 
-    const UmbraPaymaster = await ethers.getContractFactory('UmbraPaymaster', adminWallet);
-    const umbraPaymaster = await UmbraPaymaster.deploy(umbraRelayRecipient.address);
-    await umbraPaymaster.deployed();
-    save(umbraPaymaster.address, 'contracts', 'UmbraPaymaster');
-    console.log('UmbraPaymaster contract deployed to address: ', umbraPaymaster.address);
+      const UmbraPaymaster = await ethers.getContractFactory('UmbraPaymaster', adminWallet);
+      const umbraPaymaster = await UmbraPaymaster.deploy(umbraRelayRecipient.address);
+      await umbraPaymaster.deployed();
+      save(umbraPaymaster.address, 'contracts', 'UmbraPaymaster');
+      console.log('UmbraPaymaster contract deployed to address: ', umbraPaymaster.address);
 
-    // set the relayer address on the Paymaster contract
-    const setRelayHubTxReceipt = await umbraPaymaster.setRelayHub(deployParamsForNetwork.payMasterPublicRelayer);
-    setRelayHubTxReceipt.wait();
-    save(deployParamsForNetwork.payMasterPublicRelayer, 'actions', 'SetPaymasterRelayHub');
-    save(setRelayHubTxReceipt.hash, 'actions', 'SetPaymasterRelayHubTxHash');
-    console.log(
-      'UmbraPaymaster relay hub set to address: ',
-      deployParamsForNetwork.payMasterPublicRelayer,
-      ' at transaction hash:',
-      setRelayHubTxReceipt.hash
-    );
+      // set the relayer address on the Paymaster contract
+      const setRelayHubTxReceipt = await umbraPaymaster.setRelayHub(deployParamsForNetwork.payMasterPublicRelayer);
+      setRelayHubTxReceipt.wait();
+      save(deployParamsForNetwork.payMasterPublicRelayer, 'actions', 'SetPaymasterRelayHub');
+      save(setRelayHubTxReceipt.hash, 'actions', 'SetPaymasterRelayHubTxHash');
+      console.log(
+        'UmbraPaymaster relay hub set to address: ',
+        deployParamsForNetwork.payMasterPublicRelayer,
+        ' at transaction hash:',
+        setRelayHubTxReceipt.hash
+      );
 
-    const setTrustedForwaderTxReceipt = await umbraPaymaster.setTrustedForwarder(umbraForwarder.address);
-    setTrustedForwaderTxReceipt.wait();
-    save(umbraForwarder.address, 'actions', 'SetTrustedForwarder');
-    save(setTrustedForwaderTxReceipt.hash, 'actions', 'SetTrustedForwarderTxHash');
-    console.log(
-      'UmbraPaymaster trusted forwarder set to address: ',
-      umbraForwarder.address,
-      ' at transaction hash: ',
-      setTrustedForwaderTxReceipt.hash
-    );
+      const setTrustedForwaderTxReceipt = await umbraPaymaster.setTrustedForwarder(umbraForwarder.address);
+      setTrustedForwaderTxReceipt.wait();
+      save(umbraForwarder.address, 'actions', 'SetTrustedForwarder');
+      save(setTrustedForwaderTxReceipt.hash, 'actions', 'SetTrustedForwarderTxHash');
+      console.log(
+        'UmbraPaymaster trusted forwarder set to address: ',
+        umbraForwarder.address,
+        ' at transaction hash: ',
+        setTrustedForwaderTxReceipt.hash
+      );
 
-    // Create transaction to send funds to Paymaster contract
-    const tx = {
-      to: umbraPaymaster.address,
-      value: ethers.utils.parseEther('1'),
-      gasLimit: 1000000,
-    };
-    const receipt = await adminWallet.sendTransaction(tx);
-    await receipt.wait();
-    save(receipt.hash, 'actions', 'SendPaymasterFundsTx');
-    console.log(`Paymaster funding transaction successful with hash: ${receipt.hash}`);
+      // Create transaction to send funds to Paymaster contract
+      const tx = {
+        to: umbraPaymaster.address,
+        value: ethers.utils.parseEther('1'),
+        gasLimit: 1000000,
+      };
+      const receipt = await adminWallet.sendTransaction(tx);
+      await receipt.wait();
+      save(receipt.hash, 'actions', 'SendPaymasterFundsTx');
+      console.log(`Paymaster funding transaction successful with hash: ${receipt.hash}`);
+    }
 
     // everything went well, save the deployment info in the 'latest' JSON file
     fs.writeFileSync(latestFileName, JSON.stringify(parameters));
