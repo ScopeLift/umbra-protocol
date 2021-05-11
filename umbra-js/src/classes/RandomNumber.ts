58c54f900b28af146b6f088ec96a4c15a645650f
Matt Solomon <matt@mattsolomon.dev>
2021-05-11 04:51:24 -0700

    Random number fix: always use 32 bytes and remove payload extension

--- umbra-js/src/classes/RandomNumber.ts
+++ umbra-js/src/classes/RandomNumber.ts
@@ -1,54 +1,35 @@
 /**
- * @dev Class for managing random numbers. In Umbra, a random number is 32 bytes, where
- * the first 16 bytes are zeros (optionally used by developers as payload extensions), and the
- * last 16 bytes are the random number
+ * @dev Class for managing random numbers
+ * @dev In the Umbra Protocol, all random numbers should be 32 bytes to ensure sufficient security
  */
 
-import { BigNumber, hexZeroPad, isHexString } from '../ethers';
+import { BigNumber, hexZeroPad } from '../ethers';
 import { utils } from 'noble-secp256k1';
 
-const zeroPrefix = '0x00000000000000000000000000000000'; // 16 bytes of zeros
-
 export class RandomNumber {
-  readonly randomLength = 16; // 16 bytes of randomness
-  readonly fullLength = 32; // pad to number to 32 bytes
+  readonly sizeInBytes = 32; // generated random number will always be 32 bytes
   readonly value: BigNumber; // random number value
 
   /**
-   * @notice Generate a new random number
-   * @param payloadExtension 16 byte payload extension, with 0x prefix. Specify as a 34 character
-   * hex string where the first two characters are 0x
+   * @notice Generate a new 32 byte random number
    */
-  constructor(readonly payloadExtension: string = zeroPrefix) {
-    // Validate payload extension
-    if (!isHexString(payloadExtension)) {
-      throw new Error('Payload extension is not a valid hex string');
-    }
-    if (payloadExtension.length !== 34) {
-      throw new Error('Payload extension must be a 16 byte hex string with the 0x prefix');
-    }
-
-    // Generate default random number without payload extension
-    const randomNumberAsBytes = utils.randomPrivateKey(this.randomLength);
+  constructor() {
+    // Randomly generate 32 bytes and save them as a BigNumber
+    const randomNumberAsBytes = utils.randomPrivateKey(this.sizeInBytes);
     this.value = BigNumber.from(randomNumberAsBytes);
-
-    // Payload extension is valid, so update the random number. If no payload extension is provided
-    // the below is equivalent doing nothing, as it replaces the zero prefix with the zero prefix
-    const randomNumberAsHexWithPayloadExt = this.asHex.replace(zeroPrefix, payloadExtension);
-    this.value = BigNumber.from(randomNumberAsHexWithPayloadExt);
   }
 
   /**
    * @notice Get random number as hex string
    */
   get asHex() {
-    return hexZeroPad(this.value.toHexString(), this.fullLength);
+    return hexZeroPad(this.value.toHexString(), this.sizeInBytes);
   }
 
   /**
    * @notice Get random number as hex string without 0x prefix
    */
   get asHexSlim() {
-    return hexZeroPad(this.asHex, this.fullLength).slice(2);
+    return hexZeroPad(this.asHex, this.sizeInBytes).slice(2);
   }
 }
--- umbra-js/test/RandomNumber.test.ts
+++ umbra-js/test/RandomNumber.test.ts
@@ -1,11 +1,9 @@
 import { RandomNumber } from '../src/classes/RandomNumber';
 import * as chai from 'chai';
-import { BigNumber, hexZeroPad, isHexString } from '../src/ethers';
-import { randomBytes } from '@ethersproject/random';
+import { BigNumber, isHexString } from '../src/ethers';
 
 const { expect } = chai;
 const numberOfRuns = 1000; // number of runs for tests that execute in a loop
-const zeroPrefix = '00000000000000000000000000000000'; // 16 bytes of zeros
 
 describe('RandomNumber class', () => {
   let random: RandomNumber;
@@ -19,18 +17,12 @@ describe('RandomNumber class', () => {
     expect(value.constructor).to.equal(BigNumber);
   });
 
-  it('returns random value as a 32 byte hex string with 16 bytes of leading zeros', () => {
+  it('returns random value as a 32 byte hex string', () => {
     for (let i = 0; i < numberOfRuns; i += 1) {
       random = new RandomNumber();
       const hex = random.asHex;
-      const first16Bytes = hex.slice(2, 34);
-      const last16Bytes = hex.slice(34);
-
       expect(isHexString(hex)).to.be.true;
       expect(hex.length).to.equal(66); // 32 bytes plus leading 0x prefix
-      expect(first16Bytes).to.equal(zeroPrefix);
-      expect(last16Bytes).to.not.equal(zeroPrefix);
-      expect(`0x${first16Bytes}${last16Bytes}`).to.equal(hex);
     }
   });
 
@@ -38,14 +30,8 @@ describe('RandomNumber class', () => {
     for (let i = 0; i < numberOfRuns; i += 1) {
       random = new RandomNumber();
       const hex = random.asHexSlim;
-      const first16Bytes = hex.slice(0, 32);
-      const last16Bytes = hex.slice(32);
-
       expect(isHexString(hex)).to.be.false;
       expect(hex.length).to.equal(64); // 32 bytes without 0x prefix
-      expect(first16Bytes).to.equal(zeroPrefix);
-      expect(last16Bytes).to.not.equal(zeroPrefix);
-      expect(`${first16Bytes}${last16Bytes}`).to.equal(hex);
     }
   });
 
@@ -60,45 +46,5 @@ describe('RandomNumber class', () => {
 
     expect(first16Bytes).to.equal(first16BytesSlim);
     expect(last16Bytes).to.equal(last16BytesSlim);
-  });
-
-  it('throws if the payload extension is not a valid hex string', () => {
-    let badFunction;
-    const errorMessage1 = 'Payload extension is not a valid hex string';
-    const errorMessage2 = 'Payload extension must be a 16 byte hex string with the 0x prefix';
-
-    // Not a hex string
-    badFunction = () => new RandomNumber('thisShouldThrow');
-    expect(badFunction).to.throw(errorMessage1);
-
-    // Hex string of correct size, but missing 0x prefix
-    badFunction = () => new RandomNumber('ff2e6c8b7a2532a03cc70417e25084b6');
-    expect(badFunction).to.throw(errorMessage1);
-
-    // Properly formatted hex string, but wrong size
-    badFunction = () => new RandomNumber('0x123');
-    expect(badFunction).to.throw(errorMessage2);
-    badFunction = () => new RandomNumber('0xff2e6c8b7a2532a03cc70417e25084b6ff');
-    expect(badFunction).to.throw(errorMessage2);
-  });
-
-  it('lets the user set a payload extension when generating a random number', () => {
-    for (let i = 0; i < numberOfRuns; i += 1) {
-      // Generate random hex string with the correct format
-      const randomHexString = hexZeroPad(BigNumber.from(randomBytes(16)).toHexString(), 16);
-
-      random = new RandomNumber(randomHexString);
-      const hex = random.asHex;
-      const hexSlim = random.asHexSlim;
-      const first16Bytes = hexSlim.slice(0, 32);
-      const last16Bytes = hexSlim.slice(32);
-
-      expect(isHexString(hex)).to.be.true;
-      expect(hex.length).to.equal(66); // 32 bytes plus leading
-      expect(first16Bytes).to.equal(randomHexString.slice(2));
-      expect(first16Bytes).to.not.equal(zeroPrefix);
-      expect(last16Bytes).to.not.equal(randomHexString.slice(2));
-      expect(last16Bytes).to.not.equal(zeroPrefix);
-    }
   });
 });
