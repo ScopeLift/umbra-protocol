34fde6bfed06c7d90ecd9242b54d051661d0c95c
Ben DiFrancesco <ben@scopelift.co>
2021-08-16 09:37:10 -0400

    Implement and test core StealthKeyRegistry functions
    * Import an adapt ENS stealth key registry code; see
      https://github.com/ensdomains/resolvers/pull/40 for original
      context
    * Write tests for stealth registry functionality

--- contracts/contracts/StealthKeyRegistry.sol
+++ contracts/contracts/StealthKeyRegistry.sol
@@ -3,6 +3,101 @@
 pragma solidity ^0.8.7;
 
 contract StealthKeyRegistry {
+  /// @dev Event emitted when a user updates their resolver stealth keys
+  event StealthKeyChanged(
+    address indexed registrant,
+    uint256 spendingPubKeyPrefix,
+    uint256 spendingPubKey,
+    uint256 viewingPubKeyPrefix,
+    uint256 viewingPubKey
+  );
 
-  mapping(address => mapping(uint256 => uint256)) public keys;
+  /**
+   * @dev Mapping used to store two secp256k1 curve public keys useful for
+   * receiving stealth payments. The mapping records two keys: a viewing
+   * key and a spending key, which can be set and read via the `setsStealthKeys`
+   * and `stealthKey` methods respectively.
+   *
+   * The mapping associates the user's address to another mapping, which itself maps
+   * the public key prefix to the actual key . This scheme is used to avoid using an
+   * extra storage slot for the public key prefix. For a given address, the mapping
+   * may contain a spending key at position 0 or 1, and a viewing key at position
+   * 2 or 3. See the setter/getter methods for details of how these map to prefixes.
+   *
+   * For more on secp256k1 public keys and prefixes generally, see:
+   * https://github.com/ethereumbook/ethereumbook/blob/develop/04keys-addresses.asciidoc#generating-a-public-key
+   *
+   */
+  mapping(address => mapping(uint256 => uint256)) keys;
+
+  /**
+   * Sets the stealth keys associated with an address, for anonymous sends.
+   * May only be called by the associated address.
+   * @param spendingPubKeyPrefix Prefix of the spending public key (2 or 3)
+   * @param spendingPubKey The public key for generating a stealth address
+   * @param viewingPubKeyPrefix Prefix of the viewing public key (2 or 3)
+   * @param viewingPubKey The public key to use for encryption
+   */
+  function setStealthKeys(
+    uint256 spendingPubKeyPrefix,
+    uint256 spendingPubKey,
+    uint256 viewingPubKeyPrefix,
+    uint256 viewingPubKey
+  ) external {
+    require(
+      (spendingPubKeyPrefix == 2 || spendingPubKeyPrefix == 3) &&
+        (viewingPubKeyPrefix == 2 || viewingPubKeyPrefix == 3),
+      "StealthKeyResolver: Invalid Prefix"
+    );
+
+    emit StealthKeyChanged(msg.sender, spendingPubKeyPrefix, spendingPubKey, viewingPubKeyPrefix, viewingPubKey);
+
+    // Shift the spending key prefix down by 2, making it the appropriate index of 0 or 1
+    spendingPubKeyPrefix -= 2;
+
+    // Ensure the opposite prefix indices are empty
+    delete keys[msg.sender][1 - spendingPubKeyPrefix];
+    delete keys[msg.sender][5 - viewingPubKeyPrefix];
+
+    // Set the appropriate indices to the new key values
+    keys[msg.sender][spendingPubKeyPrefix] = spendingPubKey;
+    keys[msg.sender][viewingPubKeyPrefix] = viewingPubKey;
+  }
+
+  /**
+   * Returns the stealth key associated with an address.
+   * @param registrant The address whose keys to lookup.
+   * @return spendingPubKeyPrefix Prefix of the spending public key (2 or 3)
+   * @return spendingPubKey The public key for generating a stealth address
+   * @return viewingPubKeyPrefix Prefix of the viewing public key (2 or 3)
+   * @return viewingPubKey The public key to use for encryption
+   */
+  function stealthKeys(address registrant)
+    external
+    view
+    returns (
+      uint256 spendingPubKeyPrefix,
+      uint256 spendingPubKey,
+      uint256 viewingPubKeyPrefix,
+      uint256 viewingPubKey
+    )
+  {
+    if (keys[registrant][0] != 0) {
+      spendingPubKeyPrefix = 2;
+      spendingPubKey = keys[registrant][0];
+    } else {
+      spendingPubKeyPrefix = 3;
+      spendingPubKey = keys[registrant][1];
+    }
+
+    if (keys[registrant][2] != 0) {
+      viewingPubKeyPrefix = 2;
+      viewingPubKey = keys[registrant][2];
+    } else {
+      viewingPubKeyPrefix = 3;
+      viewingPubKey = keys[registrant][3];
+    }
+
+    return (spendingPubKeyPrefix, spendingPubKey, viewingPubKeyPrefix, viewingPubKey);
+  }
 }
--- contracts/test/stealth-key-registry-test.ts
+++ contracts/test/stealth-key-registry-test.ts
@@ -1,5 +1,4 @@
 import hre, { ethers } from 'hardhat';
-// const { provider } = ethers;
 const { isAddress } = ethers.utils;
 import { Artifact } from 'hardhat/types';
 
@@ -7,17 +6,19 @@ import { StealthKeyRegistry } from '../typechain';
 
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { expect } from 'chai';
-// import { parseEther } from '@ethersproject/units';
-// import { Contract } from '@ethersproject/contracts';
 
 const { deployContract } = hre.waffle;
 
+const SPENDING_KEY = 10;
+const VIEWING_KEY = 20;
+
 describe('StealthKeyRegistry', () => {
   let user: SignerWithAddress;
+  let anotherUser: SignerWithAddress;
   let registry: StealthKeyRegistry;
 
   before(async () => {
-    [user] = await hre.ethers.getSigners();
+    [user, anotherUser] = await hre.ethers.getSigners();
   });
 
   beforeEach(async () => {
@@ -27,5 +28,88 @@ describe('StealthKeyRegistry', () => {
 
   it('should deploy the registry contract', () => {
     expect(isAddress(registry.address), 'Failed to deploy StealthKeyRegistry').to.be.true;
+  });
+
+  it('should initialize with empty stealth keys', async () => {
+    const { spendingPubKey, viewingPubKey } = await registry.stealthKeys(user.address);
+
+    expect(spendingPubKey.toNumber()).to.equal(0);
+    expect(viewingPubKey.toNumber()).to.equal(0);
+  });
+
+  it('should let the user set their stealth keys', async () => {
+    await registry.setStealthKeys(2, SPENDING_KEY, 2, VIEWING_KEY);
+    const { spendingPubKeyPrefix, spendingPubKey, viewingPubKeyPrefix, viewingPubKey } = await registry.stealthKeys(
+      user.address
+    );
+
+    expect(spendingPubKeyPrefix.toNumber()).to.equal(2);
+    expect(spendingPubKey.toNumber()).to.equal(SPENDING_KEY);
+    expect(viewingPubKeyPrefix.toNumber()).to.equal(2);
+    expect(viewingPubKey.toNumber()).to.equal(VIEWING_KEY);
+  });
+
+  it('should let the user overwrite existing stealth keys', async () => {
+    // First setup
+    await registry.setStealthKeys(3, SPENDING_KEY, 2, VIEWING_KEY);
+    let { spendingPubKeyPrefix, spendingPubKey, viewingPubKeyPrefix, viewingPubKey } = await registry.stealthKeys(
+      user.address
+    );
+
+    expect(spendingPubKeyPrefix.toNumber()).to.equal(3);
+    expect(spendingPubKey.toNumber()).to.equal(SPENDING_KEY);
+    expect(viewingPubKeyPrefix.toNumber()).to.equal(2);
+    expect(viewingPubKey.toNumber()).to.equal(VIEWING_KEY);
+
+    // Overwrite existing
+    await registry.setStealthKeys(2, SPENDING_KEY + 1, 3, VIEWING_KEY + 1);
+    ({ spendingPubKeyPrefix, spendingPubKey, viewingPubKeyPrefix, viewingPubKey } = await registry.stealthKeys(
+      user.address
+    ));
+
+    expect(spendingPubKeyPrefix.toNumber()).to.equal(2);
+    expect(spendingPubKey.toNumber()).to.equal(SPENDING_KEY + 1);
+    expect(viewingPubKeyPrefix.toNumber()).to.equal(3);
+    expect(viewingPubKey.toNumber()).to.equal(VIEWING_KEY + 1);
+  });
+
+  it('should allow different users to set stealth keys independently', async () => {
+    // first user sets keys
+    await registry.connect(user).setStealthKeys(3, SPENDING_KEY, 2, VIEWING_KEY);
+    let { spendingPubKeyPrefix, spendingPubKey, viewingPubKeyPrefix, viewingPubKey } = await registry.stealthKeys(
+      user.address
+    );
+
+    expect(spendingPubKeyPrefix.toNumber()).to.equal(3);
+    expect(spendingPubKey.toNumber()).to.equal(SPENDING_KEY);
+    expect(viewingPubKeyPrefix.toNumber()).to.equal(2);
+    expect(viewingPubKey.toNumber()).to.equal(VIEWING_KEY);
+
+    // second user keys are still empty
+    ({ spendingPubKey, viewingPubKey } = await registry.stealthKeys(anotherUser.address));
+
+    expect(spendingPubKey.toNumber()).to.equal(0);
+    expect(viewingPubKey.toNumber()).to.equal(0);
+
+    // second user sets keys
+    await registry.connect(anotherUser).setStealthKeys(2, SPENDING_KEY + 1, 3, VIEWING_KEY + 1);
+    ({ spendingPubKeyPrefix, spendingPubKey, viewingPubKeyPrefix, viewingPubKey } = await registry.stealthKeys(
+      anotherUser.address
+    ));
+
+    expect(spendingPubKeyPrefix.toNumber()).to.equal(2);
+    expect(spendingPubKey.toNumber()).to.equal(SPENDING_KEY + 1);
+    expect(viewingPubKeyPrefix.toNumber()).to.equal(3);
+    expect(viewingPubKey.toNumber()).to.equal(VIEWING_KEY + 1);
+
+    // first user keys are unchanged
+    ({ spendingPubKeyPrefix, spendingPubKey, viewingPubKeyPrefix, viewingPubKey } = await registry.stealthKeys(
+      user.address
+    ));
+
+    expect(spendingPubKeyPrefix.toNumber()).to.equal(3);
+    expect(spendingPubKey.toNumber()).to.equal(SPENDING_KEY);
+    expect(viewingPubKeyPrefix.toNumber()).to.equal(2);
+    expect(viewingPubKey.toNumber()).to.equal(VIEWING_KEY);
   });
 });
