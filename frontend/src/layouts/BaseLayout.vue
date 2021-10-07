902d85e110293cb39c922aa5e221bfe01b43d34f
Matt Solomon <matt@mattsolomon.dev>
2021-10-07 13:01:25 -0700

    style: add some siren emojis to the migration notice

--- frontend/src/layouts/BaseLayout.vue
+++ frontend/src/layouts/BaseLayout.vue
@@ -89,10 +89,10 @@
           style="border-radius: 15px"
           :style="isDark ? 'color: #FFEEEE; background-color: #780A0A' : 'color: #610404; background-color: #FACDCD'"
         >
-          We've upgraded our name resolution system. Use the
+          🚨🚨🚨 We've upgraded our name resolution system. Use the
           <router-link class="hyperlink" :to="{ name: 'setup' }">Setup</router-link> page to submit one transaction
           which migrates you to the updated system. You won't be able to receive Umbra transactions until you've done
-          so. <router-link class="hyperlink" :to="{ path: '/TODO' }">Learn more</router-link>.
+          so. <router-link class="hyperlink" :to="{ path: '/TODO' }">Learn more</router-link>. 🚨🚨🚨
         </div>
       </div>
     </q-header>
