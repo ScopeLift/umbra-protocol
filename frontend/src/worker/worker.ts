5224af8ae12ab62e8383b30044fab475ff864701
Ben DiFrancesco <ben@scopelift.co>
2022-01-11 23:56:08 -0500

    WIP: convert to promises but UI still locks

--- frontend/src/worker/worker.ts
+++ frontend/src/worker/worker.ts
@@ -1,19 +1,27 @@
 import { AnnouncementDetail, UserAnnouncement, Umbra } from '@umbra/umbra-js';
 import { getAddress } from 'src/utils/ethers';
 
-export const filterUserAnnouncements = (
+export const filterUserAnnouncements = async (
   spendingPublicKey: string,
   viewingPrivateKey: string,
   announcements: AnnouncementDetail[]
 ) => {
-  const filteredAnnouncements = announcements.reduce((userAnns, ann) => {
-    const { amount, from, receiver, timestamp, token: tokenAddr, txHash } = ann;
-    const { isForUser, randomNumber } = Umbra.isAnnouncementForUser(spendingPublicKey, viewingPrivateKey, ann);
-    const token = getAddress(tokenAddr); // ensure checksummed address
-    const isWithdrawn = false; // we always assume not withdrawn and leave it to the caller to check
-    if (isForUser) userAnns.push({ randomNumber, receiver, amount, token, from, txHash, timestamp, isWithdrawn });
-    return userAnns;
-  }, [] as UserAnnouncement[]);
+  const checkPromises = announcements.map((ann) => {
+    return new Promise((resolve) => {
+      const { amount, from, receiver, timestamp, token: tokenAddr, txHash } = ann;
+      const { isForUser, randomNumber } = Umbra.isAnnouncementForUser(spendingPublicKey, viewingPrivateKey, ann);
+      const token = getAddress(tokenAddr); // ensure checksummed address
+      const isWithdrawn = false; // we always assume not withdrawn and leave it to the caller to check
+      if (isForUser) {
+        setTimeout(() => resolve({ randomNumber, receiver, amount, token, from, txHash, timestamp, isWithdrawn }), 1);
+      } else {
+        setTimeout(() => resolve(false), 1);
+      }
+    });
+  });
 
-  return filteredAnnouncements;
+  const checkedAnnouncements = await Promise.all(checkPromises);
+  const filteredAnnouncements = checkedAnnouncements.filter((ann) => !!ann);
+
+  return filteredAnnouncements as UserAnnouncement[];
 };
