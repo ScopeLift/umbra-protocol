75925dc9ac3fd35241f872973d8f919c1b2fc3a3
David Laprade <david.laprade@gmail.com>
2022-02-18 07:20:10 -0700

    Create an Avatar component

---
+++ frontend/src/components/Avatar.vue
@@ -0,0 +1,17 @@
+<template>
+  <img :src="avatar" id="avatar" width='20px' style='border-radius: 50%' />
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'Avatar',
+  props: {
+    avatar: {
+      type: (null as unknown) as PropType<string | null>,
+      required: true,
+    },
+  },
+});
+</script>
