f0fa00d56d23d6fe864e14727ba6216bea23d710
David Laprade <david.laprade@gmail.com>
2022-02-23 09:00:32 -0500

    Move Jazzicon into Avatar component

--- frontend/src/components/Avatar.vue
+++ frontend/src/components/Avatar.vue
@@ -1,15 +1,22 @@
 <template>
-  <img :src="avatar" id="avatar" width='20px' style='border-radius: 50%' />
+  <img v-if="avatar" :src="avatar" id="avatar" width='20px' style='border-radius: 50%' />
+  <Jazzicon v-else :address="address" />
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from '@vue/composition-api';
+import Jazzicon from 'src/components/Jazzicon.vue';
 
 export default defineComponent({
   name: 'Avatar',
+  components: { Jazzicon },
   props: {
     avatar: {
       type: (null as unknown) as PropType<string | null>,
+      required: false,
+    },
+    address: {
+      type: String,
       required: true,
     },
   },
