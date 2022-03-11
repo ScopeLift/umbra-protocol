9575c40457bc3cf68ce9f9a318fbcac43d8274eb
David Laprade <david.laprade@gmail.com>
2022-03-11 10:34:06 -0500

    Add a special function to format numbers after fee subtraction

--- frontend/test/utils.test.ts
+++ frontend/test/utils.test.ts
@@ -1,29 +1,32 @@
-import { humanizeTokenAmount } from '../src/utils/utils';
-import { parseUnits } from '@ethersproject/units';
+import { humanizeTokenAmount, roundReceivableAmountAfterFees } from '../src/utils/utils';
+import { parseUnits, parseEther } from '@ethersproject/units';
+
+const usdc = {
+  decimals: 6,
+  address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+  chainId: 1,
+  name: 'USDC',
+  symbol: 'USDC',
+}
+
+const weth = {
+  decimals: 18,
+  address: '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619',
+  chainId: 137,
+  name: 'WETH',
+  symbol: 'WETH',
+}
+
+const eth = {
+  decimals: 18,
+  address: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
+  chainId: 1,
+  name: 'ETH',
+  symbol: 'ETH',
+}
 
 describe('Utilities', () => {
   describe('humanizeTokenAmount', () => {
-    const usdc = {
-      decimals: 6,
-      address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
-      chainId: 1,
-      name: 'USDC',
-      symbol: 'USDC',
-    }
-    const weth = {
-      decimals: 18,
-      address: '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619',
-      chainId: 137,
-      name: 'WETH',
-      symbol: 'WETH',
-    }
-    const eth = {
-      decimals: 18,
-      address: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
-      chainId: 1,
-      name: 'ETH',
-      symbol: 'ETH',
-    }
     it('rounds USDC to 2 decimal places when > 1', () => {
       const amount = parseUnits('42.11', usdc.decimals);
       expect(humanizeTokenAmount(amount, usdc)).toEqual('42.11');
@@ -63,6 +66,45 @@ describe('Utilities', () => {
     it('eliminates unnecessary precision when zero for ETH', () => {
       const amount = parseUnits('0.0000', 'ether');
       expect(humanizeTokenAmount(amount, eth)).toEqual('0.0');
+    });
+  });
+  describe('roundReceivableAmountAfterFees', () => {
+    // the decimals of the *fee* should determine the decimals of the return val
+    it('should handle very high fees', () => {
+      //   5.0
+      // - 1.1
+      // ===========
+      //   3.9
+      const fee = parseEther('1.1');
+      const finalAmount = parseEther('3.9');
+      expect(roundReceivableAmountAfterFees(finalAmount, fee, eth)).toEqual('3.9');
+    });
+    it('should not take unnecessary decimals in the fee into account', () => {
+      //   1.0
+      // - 0.1000000
+      // ===========
+      //   0.9
+      const fee = parseEther('0.100000');
+      const finalAmount = parseEther('0.9');
+      expect(roundReceivableAmountAfterFees(finalAmount, fee, eth)).toEqual('0.9');
+    });
+    it('should sometimes add a non-significant figure', () => {
+      //   0.9999999
+      // - 0.0000099
+      // ===========
+      //   0.9999900
+      const fee = parseEther('0.0000099');
+      const finalAmount = parseEther('0.99999');
+      expect(roundReceivableAmountAfterFees(finalAmount, fee, eth)).toEqual('0.9999900');
+    });
+    it('should not always change the decimals of the final amount', () => {
+      //   2.000000
+      // - 0.000021
+      // ===========
+      //   1.999979
+      const fee = parseEther('0.000021');
+      const finalAmount = parseEther('1.999979');
+      expect(roundReceivableAmountAfterFees(finalAmount, fee, eth)).toEqual('1.999979');
     });
   });
 });
