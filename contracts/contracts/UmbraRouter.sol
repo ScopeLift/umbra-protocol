bf76649c61240dfd3a72f19d724d433a3d3420d9
Gary Ghayrat <f255eb47-1441-48a5-be8e-0a9e3193245c@anonaddy.me>
2022-03-29 10:46:40 -0400

    added comments and require

--- contracts/contracts/UmbraRouter.sol
+++ contracts/contracts/UmbraRouter.sol
@@ -2,9 +2,19 @@
 
 pragma solidity ^0.7.6;
 
-contract BatchSend {
+contract UmbraRouter {
   address internal constant umbra = 0xFb2dc580Eed955B528407b4d36FfaFe3da685401;
 
+  /**
+   * @notice Batch send ETH and token payments to a stealth address
+   * @param _receivers Stealth addresses receiving the payment
+   * @param _tokenAddrs Addresses of the ERC20 tokens being sent
+   * @param _tollCommitments Exact toll the sender is paying; should equal contract toll;
+   * the committment is used to prevent frontrunning attacks by the owner;
+   * see https://github.com/ScopeLift/umbra-protocol/issues/54 for more information
+   * @param _pkxes X-coordinate of the ephemeral public key used to encrypt the payload
+   * @param _ciphertexts Encrypted entropy (used to generated the stealth address) and payload extension
+   */
   function batchSend(
     address payable[] calldata _receivers,
     address[] calldata _tokenAddrs,
@@ -25,10 +35,11 @@ contract BatchSend {
           _ciphertexts[i]
         );
 
-        umbra.delegatecall(payload);
+        (bool success, bytes memory data) = umbra.delegatecall(payload);
+        require(success && (data.length == 0 || abi.decode(data, (bool))), "TRANSFER_FAILED");
       } else {
         payload = abi.encodeWithSignature(
-          "sendToken(address,address,uint256,bytes32, bytes32)",
+          "sendToken(address,address,uint256,bytes32,bytes32)",
           _receivers[i],
           _tokenAddrs[i],
           _amounts[i],
@@ -36,7 +47,8 @@ contract BatchSend {
           _ciphertexts[i]
         );
 
-        umbra.delegatecall(payload);
+        (bool success, bytes memory data) = umbra.delegatecall(payload);
+        require(success && (data.length == 0 || abi.decode(data, (bool))), "TRANSFER_FAILED");
       }
     }
   }
