8489ae736aea6fafbd4b7cb2cf132fa1b0f1b455
Gary Ghayrat <f255eb47-1441-48a5-be8e-0a9e3193245c@anonaddy.me>
2022-03-30 13:54:28 -0400

    added batchSendTokens

--- contracts/contracts/UmbraRouter.sol
+++ contracts/contracts/UmbraRouter.sol
@@ -2,6 +2,8 @@
 
 pragma solidity ^0.8.13;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 interface IUmbra {
   function sendEth(
     address payable receiver,
@@ -37,7 +39,7 @@ contract UmbraRouter {
     bytes32 ciphertext;
   }
 
-  function batchSendEth(uint256 _tollCommitment, SendEth[] calldata _params) external payable {
+  function batchSendEth(uint256 _tollCommitment, SendEth[] calldata _params) public payable {
     uint256 balance = msg.value;
 
     for (uint256 i = 0; i < _params.length; i++) {
@@ -60,45 +62,27 @@ contract UmbraRouter {
     }
   }
 
-  function batchSend(
-    address payable[] calldata _receivers,
-    address[] calldata _tokenAddrs,
-    uint256[] calldata _amounts,
-    uint256[] calldata _tollCommitments,
-    bytes32[] calldata _pkxes,
-    bytes32[] calldata _ciphertexts
-  ) external payable {
-    bytes memory payload;
-    uint256 ethBalance = msg.value;
+  function batchSendTokens(uint256 _tollCommitment, SendToken[] calldata _params) public payable {
+    for (uint256 i = 0; i < _params.length; i++) {
+      uint256 _amount = _params[i].amount;
+      address _tokenAddr = _params[i].tokenAddr;
+      IERC20 token = IERC20(_tokenAddr);
+      token.approve(address(this), type(uint256).max);
 
-    for (uint256 i = 0; i < _receivers.length; i++) {
-      if (_tokenAddrs[i] == address(0x0)) {
-        payload = abi.encodeWithSignature(
-          "sendEth(address,uint256,bytes32,bytes32)",
-          _receivers[i],
-          _tollCommitments[i],
-          _pkxes[i],
-          _ciphertexts[i]
-        );
-        uint256 amount = _amounts[i];
-        require(ethBalance >= amount);
-        ethBalance -= amount;
-
-        (bool success, bytes memory data) = umbra.call{value: amount}(payload);
-        require(success, "umbra delegate call failed");
-      } else {
-        payload = abi.encodeWithSignature(
-          "sendToken(address,address,uint256,bytes32,bytes32)",
-          _receivers[i],
-          _tokenAddrs[i],
-          _amounts[i],
-          _pkxes[i],
-          _ciphertexts[i]
+      // token.transferFrom(msg.sender, umbra, _amount);
+      bytes memory data =
+        abi.encodeWithSelector(
+          IUmbra.sendToken.selector,
+          _params[i].receiver,
+          _params[i].tokenAddr,
+          _amount,
+          _params[i].pkx,
+          _params[i].ciphertext
         );
 
-        (bool success, bytes memory data) = umbra.delegatecall(payload);
-        require(success && (data.length == 0 || abi.decode(data, (bool))), "DELEGATECALL_FAILED");
-      }
+      (bool success, ) = umbra.call{value: _tollCommitment}(data);
     }
   }
+
+  function batchSend() external payable {}
 }
