ceab0cf0c0c975f6e0109f429a7671696592448e
Gary Ghayrat <f255eb47-1441-48a5-be8e-0a9e3193245c@anonaddy.me>
2022-04-08 12:18:16 -0400

    added DSTestPlus in utils

---
+++ contracts-periphery/src/test/utils/DSTestPlus.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "ds-test/test.sol";
+import "forge-std/Vm.sol";
+import "forge-std/stdlib.sol";
+
+contract DSTestPlus is DSTest, stdCheats {
+    
+}
