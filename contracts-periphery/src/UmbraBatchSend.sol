f737a049d9d40c36f417b765bd260591503a39ad
Gary Ghayrat <f255eb47-1441-48a5-be8e-0a9e3193245c@anonaddy.me>
2022-04-12 13:15:53 -0400

    added SafeERC20

--- contracts-periphery/src/UmbraBatchSend.sol
+++ contracts-periphery/src/UmbraBatchSend.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 interface IUmbra {
   function sendEth(
@@ -42,7 +42,7 @@ contract UmbraBatchSend {
   error ValueMismatch();
   event Log(address indexed caller, uint256 indexed value, string message);
 
-  function batchSendEth(uint256 _tollCommitment, SendEth[] calldata _params) public payable {
+  function batchSendEth(uint256 _tollCommitment, SendEth[] calldata _params) external payable {
     uint256 valAccumulator;
 
     for (uint256 i = 0; i < _params.length; i++) {
@@ -56,7 +56,7 @@ contract UmbraBatchSend {
     _batchSendEth(_tollCommitment, _params);
   }
 
-  function batchSendTokens(uint256 _tollCommitment, SendToken[] calldata _params) public payable {
+  function batchSendTokens(uint256 _tollCommitment, SendToken[] calldata _params) external payable {
     if(msg.value != _tollCommitment * _params.length) revert ValueMismatch();
     _batchSendTokens(_tollCommitment, _params);
   }
@@ -95,10 +95,10 @@ contract UmbraBatchSend {
       address _tokenAddr = _params[i].tokenAddr;
       IERC20 token = IERC20(address(_tokenAddr));
 
-      token.transferFrom(msg.sender, address(this), _amount);
+      SafeERC20.safeTransferFrom(token, msg.sender, address(this), _amount);
 
       if (token.allowance(address(this), address(umbra)) == 0) {
-        token.approve(address(umbra), type(uint256).max);
+        SafeERC20.safeApprove(token, address(umbra), type(uint256).max);
       }
 
       umbra.sendToken{value: _tollCommitment}(
