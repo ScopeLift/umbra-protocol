2c8a496e987261243f068b9ee49246f8bf970afc
Gary Ghayrat <f255eb47-1441-48a5-be8e-0a9e3193245c@anonaddy.me>
2022-04-12 17:09:44 -0400

    added expectRevert tests

--- contracts-periphery/src/test/UmbraBatchSend.t.sol
+++ contracts-periphery/src/test/UmbraBatchSend.t.sol
@@ -13,8 +13,9 @@ contract UmbraBatchSendTest is DSTestPlus {
   using stdStorage for StdStorage;
   StdStorage stdstore;
 
-  UmbraBatchSend public router = new UmbraBatchSend();
-    
+  UmbraBatchSend router = new UmbraBatchSend();
+  
+
   address umbra = 0xFb2dc580Eed955B528407b4d36FfaFe3da685401;
   address tokenAddr = 0xc7AD46e0b8a400Bb3C915120d284AafbA8fc4735;
   address alice = address(0x202204);
@@ -33,13 +34,13 @@ contract UmbraBatchSendTest is DSTestPlus {
   UmbraBatchSend.SendEth[] sendEth;
   UmbraBatchSend.SendToken[] sendToken;
 
+  error ValueMismatch();
+
   function setUp() virtual public {
     toll = UmbraToll(umbra).toll();
     tip(tokenAddr, address(this), 1e7 ether);
     vm.deal(address(this), 1e5 ether);
   }
-
-
 
   function testFuzz_BatchSendEth(uint64 amount, uint64 amount2) public {
     vm.assume(amount > 0);
@@ -58,9 +59,17 @@ contract UmbraBatchSendTest is DSTestPlus {
     assertEq(bob.balance, bobPrevBal + amount2);
   }
 
+  function testExpectRevert_BatchSendEth() public {
+    sendEth.push(UmbraBatchSend.SendEth(payable(alice), 1 ether, pkx, ciphertext));
+    sendEth.push(UmbraBatchSend.SendEth(payable(bob), 1 ether, pkx, ciphertext));
+
+    vm.expectRevert(abi.encodeWithSelector(ValueMismatch.selector));
+    router.batchSendEth{value: 1 ether + toll}(toll, sendEth);
+
+  }
+
   function testFuzz_BatchSendTokens(uint72 amount, uint72 amount2) public {
     uint256 totalAmount = uint256(amount) + uint256(amount2);
-
 
     sendToken.push(UmbraBatchSend.SendToken(alice, tokenAddr, amount, pkx, ciphertext));
     sendToken.push(UmbraBatchSend.SendToken(bob, tokenAddr, amount2, pkx, ciphertext));
@@ -72,7 +81,16 @@ contract UmbraBatchSendTest is DSTestPlus {
     router.batchSendTokens{value: totalToll}(toll, sendToken);
 
     assertEq(token.balanceOf(umbra), umbraPrevBal + totalAmount);
-    }
+  }
+
+  function testExpectRevert_BatchSendTokens() public {
+    sendToken.push(UmbraBatchSend.SendToken(alice, tokenAddr, 1 ether, pkx, ciphertext));
+    sendToken.push(UmbraBatchSend.SendToken(bob, tokenAddr, 1 ether, pkx, ciphertext));
+
+    token.approve(address(router), 2 ether);
+    vm.expectRevert(abi.encodeWithSelector(ValueMismatch.selector));
+    router.batchSendTokens{value: toll * 2 + 42}(toll, sendToken);
+  }
 
   function testFuzz_BatchSend(uint72 amount, uint72 amount2) public {
     vm.assume(amount > 0);
@@ -97,6 +115,20 @@ contract UmbraBatchSendTest is DSTestPlus {
     assertEq(bob.balance, bobPrevBal + amount2);
     assertEq(token.balanceOf(umbra), umbraPrevBal + totalAmount);
     }
+
+  function testExpectRevert_BatchSend() public {
+    sendEth.push(UmbraBatchSend.SendEth(payable(alice), 1e16, pkx, ciphertext));
+    sendEth.push(UmbraBatchSend.SendEth(payable(bob), 1e16, pkx, ciphertext));
+
+    sendToken.push(UmbraBatchSend.SendToken(alice, tokenAddr, 1e17, pkx, ciphertext));
+    sendToken.push(UmbraBatchSend.SendToken(bob, tokenAddr, 1e17, pkx, ciphertext));
+    token.approve(address(router), 1e17 * 2);
+
+    uint256 totalToll = toll * sendEth.length + toll * sendToken.length;
+    vm.expectRevert(abi.encodeWithSelector(ValueMismatch.selector));
+    router.batchSend{value: 1e16*2 + totalToll + 42}(toll, sendEth, sendToken);
+  }
+
 }
 
 contract BatchSendWithTollTest is UmbraBatchSendTest {
