e2e51c6d81ade85a1f5dbef6bf2be1c3c987c058
Gary Ghayrat <f255eb47-1441-48a5-be8e-0a9e3193245c@anonaddy.me>
2022-05-09 18:08:18 -0400

    Keep DSTestPlus.sol

--- contracts-periphery/src/test/utils/DSTestPlus.sol
+++ contracts-periphery/src/test/utils/DSTestPlus.sol
@@ -3,13 +3,11 @@ pragma solidity ^0.8.13;
 
 import "ds-test/test.sol";
 import "forge-std/Vm.sol";
-import "forge-std/stdlib.sol";
+import "forge-std/Test.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "solmate/test/utils/mocks/MockERC20.sol";
 
-contract DSTestPlus is DSTest, stdCheats {
-  Vm vm = Vm(HEVM_ADDRESS);
-  StdStorage stdstore;
+contract DSTestPlus is DSTest, Test {
   using stdStorage for StdStorage;
 
   function setToll(address umbraAddr, uint256 newToll) public {
