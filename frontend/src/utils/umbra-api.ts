bf8ab5a6077a744da057514cbb0a783049c22f93
Matt Solomon <matt@mattsolomon.dev>
2022-06-30 07:24:26 -0700

    refactor: remove unused itx method

--- frontend/src/utils/umbra-api.ts
+++ frontend/src/utils/umbra-api.ts
@@ -5,7 +5,6 @@
 import { StaticJsonRpcProvider } from './ethers';
 import {
   FeeEstimateResponse,
-  RelayerStatusResponse,
   Provider,
   TokenInfoExtended,
   RelayResponse,
@@ -58,17 +57,6 @@ export class UmbraApi {
     const response = await fetch(url, { method: 'POST', body, headers });
     const data = (await response.json()) as RelayResponse;
     if ('error' in data) throw new Error(`Could not relay withdraw: ${data.error}`);
-    return data;
-  }
-
-  // Returns the status of the provided relayer transaction ID
-  async getRelayStatus(itxId: string) {
-    // If ChainID is 1 or 4, relaying is supported so fetch the status from the relayer. Otherwise, relaying is
-    // not supported on this chain so we throw an error
-    if (this.chainId !== 1 && this.chainId !== 4) throw new Error(`Unsupported relayer chain ID ${this.chainId}`);
-    const response = await fetch(`${this.baseUrl}/status/${itxId}?chainId=${this.chainId}`);
-    const data = (await response.json()) as RelayerStatusResponse;
-    if ('error' in data) throw new Error(`Could not get relay status: ${data.error}`);
     return data;
   }
 }
