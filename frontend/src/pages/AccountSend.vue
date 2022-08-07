ecf31c475039b40e1b32fb3adc0ac2be7ab3a597
Alex Keating <keating.dev@protonmail.com>
2022-08-07 14:34:21 -0400

    Cleaned up

--- frontend/src/pages/AccountSend.vue
+++ frontend/src/pages/AccountSend.vue
@@ -75,6 +75,7 @@
 	  <div class="flex justify-between items-center">
 		{{ $t('Send.amount') }} 
 		<base-button 
+		  v-if="recipientId"
 		  class="cursor-pointer"
 		  color="primary"
 		  label="Max"
@@ -408,42 +409,16 @@ function useSendForm() {
     sendFormRef.value?.resetValidation();
   }
 
-  /* Will require send addres */
-  /* If ethereuem show if send address */
   async function setHumanAmountMax() {
-    console.log("ahoy hoy")
-	console.log(token.value)
-	console.log(NATIVE_TOKEN.value)
+	if (!token.value?.address)  throw new Error(vm.$i18n.tc('Send.select-a-token'))
 	if (NATIVE_TOKEN.value?.address === token.value?.address) {
-		console.log("In native function")
-		/* Make sure there is an address or else raise an error*/
 		const address = await signer.value?.getAddress()
-		if (!token.value) {
-		  console.error("Missing Token")
-		  return
-		}
-		if (!address) {
-		  console.error("Missing Address")
-		  return
-		}
-		if (!recipientId.value) {
-		  console.error("Missing recipient")
-		  return
-		}
-		if (!provider.value) {
-		  console.error("Missing provider")
-		  return
-		}
-		/* Does this work for matic and other non-eth native tokens */
+		if (!address || !provider.value) throw new Error(vm.$i18n.tc('Send.wallet-not-connected'))
+		if (!recipientId.value) throw new Error(vm.$i18n.tc('Send.enter-a-recipient'))
 		const {ethToSend} = await umbraUtils.getEthSweepGasInfo(address || "", recipientId.value, provider.value)
 		humanAmount.value = formatUnits(ethToSend.toString(), token.value.decimals)
 		return ethToSend
 	}
-	if (!token.value?.address) {
-	  console.error("Missing token")
-	  return
-	}
-	console.log(balances.value[token.value.address])
 	const tokenBalance = balances.value[token.value.address]
 	return formatUnits(tokenBalance.toString(), token.value.decimals)
   }
