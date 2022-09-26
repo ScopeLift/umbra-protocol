e7bd051b2aee6721228f2cf28758b0c43b6300b2
Gary Ghayrat <f255eb47-1441-48a5-be8e-0a9e3193245c@anonaddy.me>
2022-09-26 14:16:44 -0400

    Add remapping and read permission to ci profile

--- contracts-periphery/src/UmbraBatchSend.sol
+++ contracts-periphery/src/UmbraBatchSend.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
-import "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
-import "../lib/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+import "openzeppelin-contracts/access/Ownable.sol";
+import "openzeppelin-contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-contracts/token/ERC20/utils/SafeERC20.sol";
 import "src/interface/IUmbra.sol";
 
 contract UmbraBatchSend is Ownable {
