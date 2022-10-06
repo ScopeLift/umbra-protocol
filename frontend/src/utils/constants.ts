b1bf99176e5e6e78a0fecc2226ea974ec043bdae
Matt Solomon <matt@mattsolomon.dev>
2022-10-06 15:12:08 -0700

    chore: update multicall ABI and comments

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -20,15 +20,24 @@ export const ERC20_ABI = [
 ];
 
 export const MULTICALL_ABI = [
-  // https://github.com/makerdao/multicall
-  'function aggregate(tuple(address target, bytes callData)[] calls) returns (uint256 blockNumber, bytes[] returnData)',
+  // https://github.com/mds1/multicall
+  'function aggregate(tuple(address target, bytes callData)[] calls) payable returns (uint256 blockNumber, bytes[] returnData)',
+  'function aggregate3(tuple(address target, bool allowFailure, bytes callData)[] calls) payable returns (tuple(bool success, bytes returnData)[] returnData)',
+  'function aggregate3Value(tuple(address target, bool allowFailure, uint256 value, bytes callData)[] calls) payable returns (tuple(bool success, bytes returnData)[] returnData)',
+  'function blockAndAggregate(tuple(address target, bytes callData)[] calls) payable returns (uint256 blockNumber, bytes32 blockHash, tuple(bool success, bytes returnData)[] returnData)',
+  'function getBasefee() view returns (uint256 basefee)',
   'function getBlockHash(uint256 blockNumber) view returns (bytes32 blockHash)',
+  'function getBlockNumber() view returns (uint256 blockNumber)',
+  'function getChainId() view returns (uint256 chainid)',
   'function getCurrentBlockCoinbase() view returns (address coinbase)',
   'function getCurrentBlockDifficulty() view returns (uint256 difficulty)',
   'function getCurrentBlockGasLimit() view returns (uint256 gaslimit)',
   'function getCurrentBlockTimestamp() view returns (uint256 timestamp)',
   'function getEthBalance(address addr) view returns (uint256 balance)',
   'function getLastBlockHash() view returns (bytes32 blockHash)',
+  'function tryAggregate(bool requireSuccess, tuple(address target, bytes callData)[] calls) payable returns (tuple(bool success, bytes returnData)[] returnData)',
+  'function tryBlockAndAggregate(bool requireSuccess, tuple(address target, bytes callData)[] calls) payable returns (uint256 blockNumber, bytes32 blockHash, tuple(bool success, bytes returnData)[] returnData)',
 ];
 
+// https://github.com/mds1/multicall
 export const MULTICALL_ADDRESS = '0xcA11bde05977b3631167028862bE2a173976CA11';
