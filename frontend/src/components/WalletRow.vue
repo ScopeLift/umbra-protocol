adca95cf9cb48004ad0a8a46df2603122d8d1616
Keating <keating.dev@protonmail.com>
2022-10-30 21:56:27 -0400

    Add copy translations

--- frontend/src/components/WalletRow.vue
+++ frontend/src/components/WalletRow.vue
@@ -1,14 +1,14 @@
 <template>
   <div v-if="display" class="container q-menu" :class="isDark ? 'q-dark' : ''">
     <div class="row justify-between items-center q-mb-sm">
-      <div>Account</div>
+      <div>{{ $t('WalletRow.account') }}</div>
       <div @click="setDisplayWalletRow(false)">
         <q-icon class="copy-icon q-pr-xs" name="fas fa-times" />
       </div>
     </div>
     <div class="column inner-container">
       <div class="row justify-between items-center">
-        <div class="text-xxs">Connected with {{ connectedWalletLabel }}</div>
+        <div class="text-xxs">{{ $t('WalletRow.connected-with') }}{{ connectedWalletLabel }}</div>
         <base-button
           @click="disconnectWallet()"
           label="Disconnect"
@@ -19,22 +19,22 @@
         />
       </div>
       <div class="row">
-        <div class="row text-caption text-break-word cursor-pointer copy-icon-parent">
+        <div class="row text-caption text-break-word cursor-pointer">
           <div class="flex q-mr-sm" id="wallet-row-jazzicon" />
-          <span v-if="userDisplayName" class="copy text-caption text-bold">
+          <span v-if="userDisplayName" class="text-caption text-bold">
             {{ userDisplayName }}
           </span>
         </div>
         <div class="row text-caption items-center">
           <div @click="copyAddress(userAddress)" class="text-xxs copy-icon-parent cursor-pointer">
             <q-icon class="copy-icon q-pr-xs" name="far fa-copy" />
-            <span class="text-xxs">Copy Address</span>
+            <span class="text-xxs">{{ $t('WalletRow.copy-address') }}</span>
           </div>
 
           <div class="row items-center q-ml-sm text-xxs external-link-parent cursor-pointer">
             <a class="hyperlink" :href="blockExplorerUrl" target="_blank" rel="noopener noreferrer">
               <q-icon class="external-link q-pr-xs dark-toggle" name="fas fa-external-link-alt" />
-              <span>View on Explorer</span>
+              <span>{{ $t('WalletRow.view-on-explorer') }}</span>
             </a>
           </div>
         </div>
@@ -44,30 +44,27 @@
 </template>
 
 <script lang="ts">
-// Add WalletRow
-import { defineComponent, onUpdated, computed } from '@vue/composition-api';
+import { defineComponent, onUpdated, computed, getCurrentInstance } from '@vue/composition-api';
 import { copyToClipboard } from 'quasar';
 import BaseButton from 'src/components/BaseButton.vue';
 import { toAddress } from 'src/utils/address';
+import { notifyUser } from 'src/utils/alerts';
 import useWalletStore from 'src/store/wallet';
 import jazzicon from '@metamask/jazzicon';
 import useSettingsStore from 'src/store/settings';
 
 function useWalletRow(userAddress: string) {
   const { provider, currentChain, connectedWalletLabel, disconnectWallet } = useWalletStore();
-  console.log(currentChain);
-  console.log(disconnectWallet);
-  console.log(connectedWalletLabel);
+  const vm = getCurrentInstance()!;
+
   /**
    * @notice Copies the address of type to the clipboard
    */
   async function copyAddress(address: string) {
     if (!provider.value) return;
     const mainAddress = await toAddress(address, provider.value);
-    console.log(address);
-    console.log('Click');
     await copyToClipboard(mainAddress);
-    /* notifyUser('success', `${type} ${vm.$i18n.tc('AccountReceiveTable.address-copied')}`); */
+    notifyUser('success', `${vm.$i18n.tc('WalletRow.address-copied')}`);
   }
   return {
     copyAddress,
@@ -101,8 +98,6 @@ export default defineComponent({
 
   setup(props, context) {
     const { isDark } = useSettingsStore();
-    console.log('IsDark');
-    console.log(isDark);
 
     onUpdated(() => {
       if (props.display) {
@@ -133,7 +128,6 @@ export default defineComponent({
   z-index: 1
   border-radius: 15px
   padding: 15px
-  box-shadow: 0 1px 5px rgb(0 0 0 / 20%), 0 2px 2px rgb(0 0 0 / 14%), 0 3px 1px -2px rgb(0 0 0 / 12%)
 
   @media screen and (max-width: $breakpoint-xs-max)
     left: -40px
@@ -148,6 +142,7 @@ export default defineComponent({
 
 .external-link-parent:hover
   color: $primary
+
 .text-xxs
   font-size: 10px
 </style>
