00fa26c2fadb969c30957c64bade74108df59077
garyghayrat <f255eb47-1441-48a5-be8e-0a9e3193245c@anonaddy.me>
2022-11-14 17:59:09 -0500

    Add batchSend deploy script

--- contracts-periphery/script/Deploy.s.sol
+++ contracts-periphery/script/Deploy.s.sol
@@ -2,9 +2,15 @@
 pragma solidity ^0.8.16;
 
 import "forge-std/Script.sol";
+import {UmbraBatchSend} from "src/UmbraBatchSend.sol";
+import {IUmbra} from "src/interface/IUmbra.sol";
 
 contract Deploy is Script {
+  address constant UMBRA = 0xFb2dc580Eed955B528407b4d36FfaFe3da685401;
+
   function run() public {
     vm.broadcast();
+    UmbraBatchSend batchSend = new UmbraBatchSend(IUmbra(umbraAddress));
+    console2.log("batchSend Address: ", address(batchSend));
   }
 }
