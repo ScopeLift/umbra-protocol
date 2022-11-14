0a391de0576d732ab7a10b96fe841af3b984d887
garyghayrat <f255eb47-1441-48a5-be8e-0a9e3193245c@anonaddy.me>
2022-11-14 11:46:15 -0500

    Comment out some code

--- frontend/src/pages/AccountSend.vue
+++ frontend/src/pages/AccountSend.vue
@@ -241,15 +241,7 @@
         />
       </div>
     </q-form>
-    <send-form
-      @onFormSubmit="onFormSubmit()"
-      @updateRecipientId="recipientId = arguments[0]"
-      @updateuseNormalPubKey="useNormalPubKey = arguments[0]"
-      @updateTokenPick="token= arguments[0]"
-      :recipientId="recipientId"
-      :isSending="isSending"
-      :advancedMode="advancedMode"
-    />
+
     <div style="text-align: center">------------------------------------------------</div>
     
   </q-page>
@@ -265,7 +257,6 @@ import BaseInput from 'components/BaseInput.vue';
 import BaseSelect from 'components/BaseSelect.vue';
 import BaseTooltip from 'components/BaseTooltip.vue';
 import ConnectWallet from 'components/ConnectWallet.vue';
-import SendForm from 'components/SendForm.vue';
 // --- Store ---
 import useSettingsStore from 'src/store/settings';
 import useWalletStore from 'src/store/wallet';
@@ -582,7 +573,7 @@ function useSendForm() {
 
 export default defineComponent({
   name: 'PageSend',
-  components: { BaseTooltip, ConnectWallet, SendForm },
+  components: { BaseTooltip, ConnectWallet },
   setup() {
     const isMaintenanceMode = Number(process.env.MAINTENANCE_MODE_SEND) === 1;
     return { generatePaymentLink, ...useSendForm(), isMaintenanceMode };
