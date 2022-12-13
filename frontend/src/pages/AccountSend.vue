75b312e4515ec8d63cc56d8d635fab1359c09455
Matt Solomon <matt@mattsolomon.dev>
2022-12-13 11:17:47 -0800

    style: prettier

--- frontend/src/pages/AccountSend.vue
+++ frontend/src/pages/AccountSend.vue
@@ -272,7 +272,7 @@ function useSendForm() {
       // Reset token and amount if token is not supported on the network
       if (
         tokenList.value.length &&
-        !tokenList.value.some(tokenOption => tokenOption.symbol === (tokenValue as TokenInfoExtended)?.symbol)
+        !tokenList.value.some((tokenOption) => tokenOption.symbol === (tokenValue as TokenInfoExtended)?.symbol)
       ) {
         token.value = tokenList.value[0];
         humanAmount.value = undefined;
@@ -328,7 +328,7 @@ function useSendForm() {
   const isNativeToken = (address: string) => getAddress(address) === NATIVE_TOKEN.value.address;
 
   const getMinSendAmount = (tokenAddress: string): number => {
-    const tokenInfo = tokenList.value.filter(token => token.address === tokenAddress)[0];
+    const tokenInfo = tokenList.value.filter((token) => token.address === tokenAddress)[0];
     if (!tokenInfo) throw new Error(`token info unavailable for ${tokenAddress}`); // this state should not be possible
     const tokenMinSendInWei = parseUnits(tokenInfo.minSendAmount, 'wei');
     // We don't need to worry about fallbacks: native tokens have hardcoded fallbacks
