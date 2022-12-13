eda3454046982a455eb43f37ed738c84368a91e1
Matt Solomon <matt@mattsolomon.dev>
2022-12-13 15:31:52 -0800

    fix: errors in i18n boot file

--- frontend/src/boot/i18n.ts
+++ frontend/src/boot/i18n.ts
@@ -1,12 +1,10 @@
 import { boot } from 'quasar/wrappers';
-import Quasar from 'quasar';
 import { createI18n } from 'vue-i18n';
 import messages from 'src/i18n';
 
 // Configure and create the i18n instance. Exported so it can be imported within files and components.
 const params = new URLSearchParams(window.location.search);
-const locale = params.get('locale') || Quasar.lang.getLocale();
-window.logger.info(params.get('locale'));
+const locale = params.get('locale') || 'en-US';
 
 export const i18n = createI18n({
   locale,
