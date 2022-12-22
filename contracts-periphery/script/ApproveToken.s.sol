91ef284bf120a7e276916463218f39ad5f1d09dd
Gary Ghayrat <f255eb47-1441-48a5-be8e-0a9e3193245c@anonaddy.me>
2022-12-22 12:30:40 -0500

    Run forge fmt

--- contracts-periphery/script/ApproveToken.s.sol
+++ contracts-periphery/script/ApproveToken.s.sol
@@ -6,12 +6,11 @@ import {UmbraBatchSend} from "src/UmbraBatchSend.sol";
 import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
 
 contract ApproveTokenScript is Script {
+  UmbraBatchSend batchSend = UmbraBatchSend(0x0d81Df222BB44b883265538586829715CF157163);
+  address token = 0x57Cf115b0Abdce35dC9A8A8D9DfF1f586eA63ec8; //USDC
 
-    UmbraBatchSend batchSend = UmbraBatchSend(0x0d81Df222BB44b883265538586829715CF157163);
-    address token = 0x57Cf115b0Abdce35dC9A8A8D9DfF1f586eA63ec8; //USDC
-
-    function run() public {
-        vm.broadcast();
-        batchSend.approveToken(IERC20(address(token)));
-    }
+  function run() public {
+    vm.broadcast();
+    batchSend.approveToken(IERC20(address(token)));
+  }
 }
