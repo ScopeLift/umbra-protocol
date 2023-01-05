971aa538f3fa5d99e66083d6bc7b7617476159dc
David Laprade <david.laprade@gmail.com>
2023-01-05 12:47:14 -0500

    Quasar v2 Fix Sends and Setup (#433)
    * Don't let Vue refs turn ethers objects into proxies
    
    * Prevent setup from erroring
    
    Error was:
      "proxy must report the same value for the non-writable, non-configurable property '"interface"'
    
    * Shamelessly copy dgrants note to improve our markRaw comment

--- frontend/src/store/wallet.ts
+++ frontend/src/store/wallet.ts
@@ -1,4 +1,4 @@
-import { computed, onMounted, ref, watch } from 'vue';
+import { computed, onMounted, markRaw, ref, watch } from 'vue';
 
 import Onboard, { OnboardAPI } from '@web3-onboard/core';
 import injectedModule from '@web3-onboard/injected-wallets';
@@ -264,9 +264,23 @@ export default function useWalletStore() {
         return;
       }
 
-      // Set Umbra and StealthKeyRegistry classes
-      umbra.value = new Umbra(provider.value, newChainId);
-      stealthKeyRegistry.value = new StealthKeyRegistry(signer.value);
+      // Set Umbra and StealthKeyRegistry classes.
+      //
+      // When assigning ethers objects as refs, we must wrap the object in `markRaw` for assignment. This wasn't required
+      // by Vue 2's reactivity system based on Object.defineProperty, but is required for Vue 3's reactivity system based
+      // on ES6 proxies. The Vue 3 reactivity system does not work well with non-configurable, non-writable properties on
+      // objects, and many ethers classes, such as providers and networks, use non-configurable or non-writable properties.
+      // Therefore we wrap the object in `markRaw` to prevent it from being converted to a Proxy. If you do not do this,
+      // you'll see errors like this when using ethers objects as refs:
+      //     Uncaught (in promise) TypeError: 'get' on proxy: property 'interface' is a read-only and non-configurable data
+      //     property on the proxy target but the proxy did not return its actual value (expected '#<Object>' but got
+      //     '[object Object]')
+      // Read more here:
+      //     - https://github.com/vuejs/vue-next/issues/3024
+      //     - https://stackoverflow.com/questions/65693108/threejs-component-working-in-vuejs-2-but-not-3
+      //     - https://v3.vuejs.org/api/basic-reactivity.html#markraw
+      umbra.value = markRaw(new Umbra(provider.value, newChainId));
+      stealthKeyRegistry.value = markRaw(new StealthKeyRegistry(signer.value));
 
       // Setup to check if user is connected with Argent, since we need to handle a few things differently in that case.
       // When using Argent, a user's funds are secured by the contract wallet, not by a private key. As a result:
