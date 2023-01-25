4efce5a53545e6eb264cec8f00644abc9f7cbdfa
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2023-01-25 02:05:56 +0100

    fix config

--- inlang.config.js
+++ inlang.config.js
@@ -1,31 +1,20 @@
-/**
- * The inlang config is the entry point for every inlang app. 
- * 
- * More information about the config is available here 
- * https://inlang.com/documentation/config
- */
+// filename: inlang.config.js
+
 export async function initializeConfig(env) {
-    // importing inlang-plugin-json from https://github.com/samuelstroschein/inlang-plugin-json
-	const plugin = await env.$import(
-		"https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@1.0.0/dist/index.js"
-	);
-
-	const pluginConfig = {
-		pathPattern: "./frontend/src/i18n/locales/{language}.json",
-	};
-
-	return {
-		referenceLanguage: "en-us",
-		languages: [
-			"en-us",
-			"zh-cn",
-			
-		],
-		readResources: (args) => {
-			return plugin.readResources({ ...args, ...env, pluginConfig });
-		},
-		writeResources: (args) => {
-			return plugin.writeResources({ ...args, ...env, pluginConfig });
-		},
-	};
-}
+    const plugin = await env.$import(
+      "https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@1/dist/index.js"
+    );
+  
+    const pluginConfig = {
+      pathPattern: "./{language}.json",
+    };
+  
+    return {
+      referenceLanguage: "en-us",
+      languages: ["en-us", "zh-cn"],
+      readResources: (args) =>
+        plugin.readResources({ ...args, ...env, pluginConfig }),
+      writeResources: (args) =>
+        plugin.writeResources({ ...args, ...env, pluginConfig }),
+    };
+  }
