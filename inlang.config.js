8aa1ea8b8157169d4793dda0f5049e871dbd3d98
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2023-01-26 21:21:13 +0100

    init inlang.config

---
+++ inlang.config.js
@@ -0,0 +1,20 @@
+//https://inlang.com/documentation
+export async function defineConfig(env) {
+
+    const plugin = await env.$import(
+      "https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@1/dist/index.js"
+    );
+  
+    const pluginConfig = {
+      pathPattern: "./frontend/src/i18n/locales/{language}.json",
+    };
+  
+    return {
+      referenceLanguage: "en-us",
+      languages: ["en-us", "zh-cn"],
+      readResources: (args) =>
+        plugin.readResources({ ...args, ...env, pluginConfig }),
+      writeResources: (args) =>
+        plugin.writeResources({ ...args, ...env, pluginConfig }),
+    };
+  }
