490383a1f16e28bc5f40e7bc51a05afd8a715012
Matt Solomon <matt@mattsolomon.dev>
2023-02-09 10:51:31 -0800

    chore: bump version

--- frontend/src/boot/logger.ts
+++ frontend/src/boot/logger.ts
@@ -10,6 +10,6 @@ import { LogLevel } from 'src/utils/ethers';
 // Source: https://github.com/ethers-io/ethers.js/blob/d395d16fa357ec5dda9b59922cf21c39dc34c071/packages/logger/src.ts/index.ts#L44-L49
 UmbraLogger.setLogLevel((process.env.LOG_LEVEL as LogLevel) || LogLevel.DEBUG);
 
-const version = '2.0.0'; // must match package.json version
+const version = '2.0.1'; // must match package.json version
 const logger = new UmbraLogger(version);
 window.logger = logger;
