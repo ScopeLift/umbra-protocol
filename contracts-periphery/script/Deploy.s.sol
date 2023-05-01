c9dc064a58fd61f49aef59ee1041bf0e164eb653
garyghayrat <gary.ghayrat@pm.me>
2023-05-01 12:21:17 -0400

    Update forge-std and script

--- contracts-periphery/script/Deploy.s.sol
+++ contracts-periphery/script/Deploy.s.sol
@@ -1,15 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
-import "forge-std/Script.sol";
+import {Script} from "forge-std/Script.sol";
+import {StdChains} from "forge-std/StdChains.sol";
 import {UmbraBatchSend} from "src/UmbraBatchSend.sol";
 import {IUmbra} from "src/interface/IUmbra.sol";
 
 contract Deploy is Script {
   address constant UMBRA = 0xFb2dc580Eed955B528407b4d36FfaFe3da685401;
 
-  function run() public {
+  function deploy() internal {
     vm.broadcast();
     new UmbraBatchSend(IUmbra(UMBRA));
+  }
+}
+
+contract ScriptBase is Deploy {
+  string[] public networks = ["mainnet", "optimism", "arbitrum_one", "polygon", "goerli", "sepolia"];
+
+  function run(address expectedContractAddress) public {
+    for (uint256 i; i < networks.length; i++) {
+      setFallbackToDefaultRpcUrls(false);
+      vm.createSelectFork(getChain(networks[i]).rpcUrl);
+      bool isDeployed = address(expectedContractAddress).code.length > 0;
+      if (!isDeployed) deploy();
+    }
   }
 }
