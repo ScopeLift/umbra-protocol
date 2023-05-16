c3995236956cb68d2bf47575113a046458746483
Matt Solomon <matt@mattsolomon.dev>
2023-05-16 13:45:39 -0700

    chore: update natspec

--- contracts-periphery/src/UmbraBatchSend.sol
+++ contracts-periphery/src/UmbraBatchSend.sol
@@ -6,25 +6,35 @@ import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "openzeppelin-contracts/token/ERC20/utils/SafeERC20.sol";
 import {IUmbra} from "src/interface/IUmbra.sol";
 
+/// @notice This contract allows for batch sending ETH and tokens via Umbra.
 contract UmbraBatchSend is Ownable {
   using SafeERC20 for IERC20;
 
+  /// @dev Special address used to indicate the chain's native asset, e.g. ETH.
   address internal constant ETH = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
-  IUmbra internal immutable UMBRA;
 
+  /// @notice Address of the Umbra contract.
+  IUmbra public immutable UMBRA;
+
+  /// @dev Data for a single payment.
   struct SendData {
     address receiver; // Stealth address.
-    address tokenAddr; // Use 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE for chain's native asset.
+    address tokenAddr; // Token being sent, or the above `ETH` address for the chain's native asset.
     uint256 amount; // Amount of the token to send, excluding the toll.
     bytes32 pkx; // Ephemeral public key x coordinate.
     bytes32 ciphertext; // Encrypted entropy.
   }
 
+  /// @dev Emitted on a successful batch send.
   event BatchSendExecuted(address indexed sender);
 
+  /// @dev Thrown when the array of SendData structs is not sorted by token address.
   error NotSorted();
+
+  /// @dev Thrown when too much ETH was sent to the contract.
   error TooMuchEthSent();
 
+  /// @param _umbra Address of the Umbra contract.
   constructor(IUmbra _umbra) {
     UMBRA = _umbra;
   }
@@ -32,7 +42,7 @@ contract UmbraBatchSend is Ownable {
   /// @notice Batch send ETH and tokens via Umbra.
   /// @param _tollCommitment The toll commitment to use for all payments.
   /// @param _data Array of SendData structs, each containing the data for a single payment.
-  /// Must be sorted by token address, with 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE used as
+  /// Must be sorted by token address, with `0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE` used as
   /// the token address for the chain's native asset.
   function batchSend(uint256 _tollCommitment, SendData[] calldata _data) external payable {
     uint256 _initEthBalance = address(this).balance; // Includes ETH from msg.value.
@@ -74,17 +84,19 @@ contract UmbraBatchSend is Ownable {
     emit BatchSendExecuted(msg.sender);
   }
 
+  /// @dev Pulls the specified amount of the given token into this contract, and is a no-op for
+  /// the native token.
   function _pullToken(address _tokenAddr, uint256 _amount) internal {
     if (_tokenAddr != ETH) IERC20(_tokenAddr).safeTransferFrom(msg.sender, address(this), _amount);
   }
 
   /// @notice Whenever a new token is added to Umbra, this method must be called by the owner to
-  /// support
-  /// that token in this contract.
+  /// support that token in this contract.
   function approveToken(IERC20 _token) external onlyOwner {
     _token.safeApprove(address(UMBRA), type(uint256).max);
   }
 
+  /// @dev Increments a uint256 without reverting on overflow.
   function _uncheckedIncrement(uint256 i) internal pure returns (uint256) {
     unchecked {
       return i + 1;
