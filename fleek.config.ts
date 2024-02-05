9029d427f211dd62b1a1fb00f8aa00061a3caebf
Alexander Keating <keating.dev@protonmail.com>
2024-02-05 16:48:08 -0500

    Support deploying to fleek(#638)
    * Add support for decentralized hosting

---
+++ fleek.config.ts
@@ -0,0 +1,10 @@
+import { FleekConfig } from '@fleekxyz/cli';
+
+export default {
+  "sites": [
+    {
+      "slug": "many-island-colossal",
+      "distDir": "./frontend/dist/spa"
+    }
+  ]
+} satisfies FleekConfig;
