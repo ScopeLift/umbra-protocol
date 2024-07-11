b6278897deab200b572900b6ffd6ad76fb4d8d85
marcomariscal <marco.a.mariscal@gmail.com>
2024-07-11 11:13:12 -0700

    fix: img

--- frontend/src/components/AccountReceiveTable.vue
+++ frontend/src/components/AccountReceiveTable.vue
@@ -132,8 +132,12 @@
           <div :key="props.row.id" class="col-12">
             <q-card class="card-border cursor-pointer q-pt-md col justify-center items-center">
               <q-card-section class="row justify-center items-center">
-                <img v-if="getTokenInfo(props.row.token)" src="path/to/image.jpg" />props.row.token, tokens)"
-                style="width: 1.2rem" v-if="getTokenInfo(props.row.token)" />
+                <img
+                  class="q-mr-md"
+                  :src="getTokenLogoUri(props.row.token, tokens)"
+                  style="width: 1.2rem"
+                  v-if="getTokenInfo(props.row.token)"
+                />
                 <div class="text-primary text-h6 header-black q-pb-none">
                   {{ formatAmount(props.row.amount, props.row.token, tokens) }}
                   {{ getTokenSymbol(props.row.token, tokens) }}
