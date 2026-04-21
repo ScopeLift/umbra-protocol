6ab78bea74f94f10898f52b57b07c9fef996019d
Ben DiFrancesco <ben@scopelift.co>
2026-04-21 06:31:03 -0400

    Add env-var driven global Umbra maintenance mode

---
+++ frontend/src/pages/Maintenance.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="fullscreen text-center q-pa-md flex flex-center">
+    <div class="text-h5" style="max-width: 600px">
+      Sorry, the Umbra frontend is down for maintenance. Please check back soon.
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageMaintenance',
+});
+</script>
